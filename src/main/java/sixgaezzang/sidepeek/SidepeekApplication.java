cab16580bb5420169859a23cc6830c52ea52f494
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-02-06 12:50:22 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/sixgaezzang/sidepeek/SidepeekApplication.java
@@ -0,0 +1,13 @@
+package sixgaezzang.sidepeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidepeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidepeekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sixgaezzang/sidepeek/SidepeekApplicationTests.java
@@ -0,0 +1,13 @@
+package sixgaezzang.sidepeek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidepeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
