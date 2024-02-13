a84b0d899566f62ddeecb10f2eeae0b40b088a41
uijin-j <uijin0531@gmail.com>
2024-02-14 03:12:29 +0900

    request: 회원가입 요청 DTO 생성

---
+++ src/main/java/sixgaezzang/sidepeek/users/dto/SignUpRequest.java
@@ -0,0 +1,21 @@
+package sixgaezzang.sidepeek.users.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
+import sixgaezzang.sidepeek.common.ValidationUtils;
+
+public record SignUpRequest(
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    String email,
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    @Pattern(regexp = ValidationUtils.PASSWORD_REGXP, message = "비밀번호는 8자 이상이며 영문, 숫자, 특수문자를 포함해야 합니다.")
+    String password,
+    @NotBlank(message = "닉네임을 입력해주세요.")
+    @Length(max = 20, message = "닉네임은 20자 이하여야 합니다.")
+    String nickname
+) {
+
+}
