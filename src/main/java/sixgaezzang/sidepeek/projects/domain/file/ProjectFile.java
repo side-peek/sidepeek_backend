8ae82624e328e4588340af12d3994c1366816822
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-02-14 00:08:31 +0900

    feat: ProjectFile 단방향 매핑 설정

--- src/main/java/sixgaezzang/sidepeek/projects/domain/file/ProjectFile.java
+++ src/main/java/sixgaezzang/sidepeek/projects/domain/file/ProjectFile.java
@@ -4,14 +4,17 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import sixgaezzang.sidepeek.projects.domain.Project;
 
 @Entity
 @Table(name = "files")
@@ -23,6 +26,10 @@ public class ProjectFile {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
+    private Project project;
+
     @Column(name = "type", nullable = false, length = 30, columnDefinition = "VARCHAR(30)")
     @Enumerated(EnumType.STRING)
     private FileType type;
@@ -30,11 +37,5 @@ public class ProjectFile {
     @Column(name = "url", nullable = false, columnDefinition = "TEXT")
     private String url;
 
-    @Builder
-    public ProjectFile(Long id, FileType type, String url) {
-        this.id = id;
-        this.type = type;
-        this.url = url;
-    }
-
+    // 생성자
 }
