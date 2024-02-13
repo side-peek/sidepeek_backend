6beb43eafbd366d884bd97fc901f8fb009ae5a9a
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-02-14 00:29:14 +0900

    feat: ProjectSkill 클래스 정의

---
+++ src/main/java/sixgaezzang/sidepeek/projects/domain/ProjectSkill.java
@@ -0,0 +1,38 @@
+package sixgaezzang.sidepeek.projects.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sixgaezzang.sidepeek.common.Skill;
+
+@Entity
+@Table(name = "project_skill")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class ProjectSkill {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "skill_id")
+    private Skill skill;
+
+    @Column(name = "category", nullable = false, length = 50)
+    private String category;
+
+}
