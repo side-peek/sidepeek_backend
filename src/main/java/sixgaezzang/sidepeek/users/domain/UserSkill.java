0c791b45e104116e4ac9430136e6010ce29ca9e9
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-02-14 00:29:21 +0900

    feat: UserSkill 클래스 정의

---
+++ src/main/java/sixgaezzang/sidepeek/users/domain/UserSkill.java
@@ -0,0 +1,38 @@
+package sixgaezzang.sidepeek.users.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sixgaezzang.sidepeek.common.Skill;
+
+@Entity
+@Table(name = "user_skill")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class UserSkill {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "skill_id")
+    private Skill skill;
+
+    @Column(name = "category", nullable = false, length = 50)
+    private String category;
+
+}
