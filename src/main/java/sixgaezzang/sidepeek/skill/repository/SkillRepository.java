eb1c3a8bfbe8266a6d5387da2de01b307e51a6a3
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-02-14 18:25:20 +0900

    refctor: 변수명 수정

--- src/main/java/sixgaezzang/sidepeek/skill/repository/SkillRepository.java
+++ src/main/java/sixgaezzang/sidepeek/skill/repository/SkillRepository.java
@@ -6,5 +6,5 @@ import sixgaezzang.sidepeek.skill.domain.Skill;
 
 public interface SkillRepository extends JpaRepository<Skill, Long> {
 
-    List<Skill> findAllByNameContaining(String keyword);
+    List<Skill> findAllByNameContaining(String searchKeyword);
 }
