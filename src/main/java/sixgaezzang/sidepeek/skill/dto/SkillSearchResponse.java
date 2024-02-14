63407901e10b056a8e560a2f0b413c9f977b8a25
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-02-14 22:17:11 +0900

    feat: Skill 응답 dto 적용

--- src/main/java/sixgaezzang/sidepeek/skill/dto/SkillSearchResponse.java
+++ src/main/java/sixgaezzang/sidepeek/skill/dto/SkillSearchResponse.java
@@ -1,9 +1,8 @@
 package sixgaezzang.sidepeek.skill.dto;
 
 import java.util.List;
-import sixgaezzang.sidepeek.skill.domain.Skill;
 
 public record SkillSearchResponse(
-    List<Skill> skills
+    List<SkillResponse> skills
 ) {
 }
--- src/main/java/sixgaezzang/sidepeek/skill/serivce/SkillService.java
+++ src/main/java/sixgaezzang/sidepeek/skill/serivce/SkillService.java
@@ -2,9 +2,12 @@ package sixgaezzang.sidepeek.skill.serivce;
 
 import static sixgaezzang.sidepeek.common.ValidationUtils.validateMaxLength;
 
+import java.util.List;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import sixgaezzang.sidepeek.skill.domain.Skill;
+import sixgaezzang.sidepeek.skill.dto.SkillResponse;
 import sixgaezzang.sidepeek.skill.dto.SkillSearchResponse;
 import sixgaezzang.sidepeek.skill.repository.SkillRepository;
 
@@ -17,11 +20,18 @@ public class SkillService {
     private final SkillRepository skillRepository;
 
     public SkillSearchResponse searchByName(String searchKeyword) {
+        List<Skill> skillList;
         if (Objects.isNull(searchKeyword) || searchKeyword.isBlank()) {
-            return new SkillSearchResponse(skillRepository.findAll());
+            skillList = skillRepository.findAll();
+        } else {
+            validateMaxLength(searchKeyword, KEYWORD_MAX_LENGTH, "최대 " + KEYWORD_MAX_LENGTH + "자의 키워드로 검색할 수 있습니다.");
+            skillList = skillRepository.findAllByNameContaining(searchKeyword);
         }
 
-        validateMaxLength(searchKeyword, KEYWORD_MAX_LENGTH, "최대 " + KEYWORD_MAX_LENGTH + "자의 키워드로 검색할 수 있습니다.");
-        return new SkillSearchResponse(skillRepository.findAllByNameContaining(searchKeyword));
+        List<SkillResponse> response = skillList.stream()
+            .map(skill -> new SkillResponse(skill.getId(), skill.getName(), skill.getIconImageUrl()))
+            .toList();
+
+        return new SkillSearchResponse(response);
     }
 }
