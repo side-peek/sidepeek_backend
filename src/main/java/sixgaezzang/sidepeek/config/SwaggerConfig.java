3200e005ee81e0e70094a4950e11e73aa9d16921
uijin-j <uijin0531@gmail.com>
2024-02-15 02:47:35 +0900

    feat: SwaggerConfig 클래스 작성

---
+++ src/main/java/sixgaezzang/sidepeek/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package sixgaezzang.sidepeek.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+            .components(new Components())
+            .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+            .title("Sidepeek API")
+            .description("사이드픽 API 명세서 \uD83D\uDC40")
+            .version("v1");
+    }
+}
