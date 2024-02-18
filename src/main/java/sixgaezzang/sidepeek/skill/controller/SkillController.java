fdcbd122aa9ff2f0a85188d7dacee72f8e860d7c
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-02-18 19:19:07 +0900

    feat: skill 컨트롤러 스웨거 추가 설명 적용

--- src/main/java/sixgaezzang/sidepeek/skill/controller/SkillController.java
+++ src/main/java/sixgaezzang/sidepeek/skill/controller/SkillController.java
@@ -1,5 +1,8 @@
 package sixgaezzang.sidepeek.skill.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +21,9 @@ public class SkillController {
     private final SkillService skillService;
 
     @GetMapping
+    @Operation(summary = "기술 스택 검색")
+    @ApiResponse(responseCode = "200", description = "기술 스택 검색 성공")
+    @Parameter(name = "keyword", description = "검색어", example = "spring")
     public ResponseEntity<SkillSearchResponse> searchByName(
         @RequestParam(required = false)
         @Size(max = 50, message = "최대 50자의 키워드로 검색할 수 있습니다.")
