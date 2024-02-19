6b638d8aa0b7a73cf98b37919ff0e050400cafa8
EomYerim <djadpfa1002@naver.com>
2024-02-20 04:50:44 +0900

    feat: 프로젝트 상세 조회 관련 응답 DTO 생성

---
+++ src/main/java/sixgaezzang/sidepeek/projects/dto/response/ProjectResponse.java
@@ -0,0 +1,48 @@
+package sixgaezzang.sidepeek.projects.dto.response;
+
+import java.time.YearMonth;
+import java.util.List;
+import lombok.Builder;
+import sixgaezzang.sidepeek.projects.domain.Project;
+
+@Builder
+public record ProjectResponse(
+    Long id,
+    String name,
+    String subName,
+    String overview,
+    String thumbnailUrl,
+    String githubUrl,
+    String deployUrl,
+    Long viewCount,
+    Long likeCount,
+    List<ProjectSkillSummary> techStacks,
+    YearMonth startDate,
+    YearMonth endDate,
+    List<MemberSummary> members,
+    String description,
+    String troubleShooting
+) {
+
+    public static ProjectResponse from(Project project, List<ProjectSkillSummary> techStacks,
+        List<MemberSummary> members) {
+        return ProjectResponse.builder()
+            .id(project.getId())
+            .name(project.getName())
+            .subName(project.getSubName())
+            .overview(project.getOverview())
+            .thumbnailUrl(project.getThumbnailUrl())
+            .githubUrl(project.getGithubUrl())
+            .deployUrl(project.getDeployUrl())
+            .viewCount(project.getViewCount())
+            .likeCount(project.getLikeCount())
+            .techStacks(techStacks)
+            .startDate(YearMonth.from(project.getStartDate()))
+            .endDate(YearMonth.from(project.getEndDate()))
+            .members(members)
+            .description(project.getDescription())
+            .troubleShooting(project.getTroubleshooting())
+            .build();
+    }
+
+}
