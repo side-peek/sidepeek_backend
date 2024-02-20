b5d1b43d64e924ee702b9441a5c65ee8fab5d2ac
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-02-20 15:20:35 +0900

    feat: Project 객체로 바꾸는 toEntity 메서드 구현

--- src/main/java/sixgaezzang/sidepeek/projects/dto/request/ProjectSaveRequest.java
+++ src/main/java/sixgaezzang/sidepeek/projects/dto/request/ProjectSaveRequest.java
@@ -1,9 +1,39 @@
 package sixgaezzang.sidepeek.projects.dto.request;
 
-import lombok.Builder;
+import java.time.LocalDateTime;
+import java.util.List;
+import sixgaezzang.sidepeek.projects.domain.Project;
 
-@Builder
 public record ProjectSaveRequest(
+    String name,
+    String subName,
+    String overview,
+    String thumbnailUrl,
+    String githubUrl,
+    String deployUrl,
+    LocalDateTime startDate,
+    LocalDateTime endDate,
+    Long ownerId,
+    String description,
+    String troubleShooting,
 
+    List<OverviewImageSaveRequest> overviewImageUrl,
+    List<ProjectSkillSaveRequest> techStacks,
+    List<MemberSaveRequest> members
 ) {
+    public Project toEntity() {
+        return Project.builder()
+            .name(this.name)
+            .subName(this.subName)
+            .overview(this.overview)
+            .thumbnailUrl(this.thumbnailUrl)
+            .githubUrl(this.githubUrl)
+            .deployUrl(this.deployUrl)
+            .startDate(this.startDate)
+            .endDate(this.endDate)
+            .ownerId(this.ownerId)
+            .description(this.description)
+            .troubleshooting(this.troubleShooting)
+            .build();
+    }
 }
