f1fd0bbbfc00c0d387e8231a1199fb9bcdd9ebc3
uijin-j <uijin0531@gmail.com>
2024-02-21 02:08:05 +0900

    feat: 로그인 컨트롤러 Swagger 추가

--- src/main/java/sixgaezzang/sidepeek/auth/controller/AuthController.java
+++ src/main/java/sixgaezzang/sidepeek/auth/controller/AuthController.java
@@ -1,5 +1,9 @@
 package sixgaezzang.sidepeek.auth.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +23,12 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/login")
+    @Operation(summary = "로그인")
+    @ApiResponse(responseCode = "200", description = "로그인 성공")
+    @Parameters({
+        @Parameter(name = "email", description = "이메일", example = "sidepeek6@gmail.com"),
+        @Parameter(name = "password", description = "비밀번호", example = "sidepeek6!")
+    })
     public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest request) {
         LoginResponse response = authService.login(request);
 
