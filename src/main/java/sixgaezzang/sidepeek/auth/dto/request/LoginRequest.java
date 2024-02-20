eb25e63a23de951a2f4d3e39fe5f465cb8050b7d
uijin-j <uijin0531@gmail.com>
2024-02-21 00:38:57 +0900

    feat: 로그인 요청/수락 DTO 생성

---
+++ src/main/java/sixgaezzang/sidepeek/auth/dto/request/LoginRequest.java
@@ -0,0 +1,12 @@
+package sixgaezzang.sidepeek.auth.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(
+    @NotBlank
+    String email,
+    @NotBlank
+    String password
+) {
+
+}
