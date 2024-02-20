1d057419fd6022282c84451ae8d9fbae2335b4d3
uijin-j <uijin0531@gmail.com>
2024-02-21 00:32:05 +0900

    refactor: 불필요한 애너테이션 제거

--- src/main/java/sixgaezzang/sidepeek/config/SecurityConfig.java
+++ src/main/java/sixgaezzang/sidepeek/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package sixgaezzang.sidepeek.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,7 +11,6 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
