aabbaa136ed2fd59e0cef0c25d61037c3054a1c2
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-02-20 14:11:01 +0900

    refactor: 불필요한 레포지토리 삭제

--- src/main/java/sixgaezzang/sidepeek/projects/repository/ProjectFileRepository.java
+++
@@ -1,7 +0,0 @@
-package sixgaezzang.sidepeek.projects.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sixgaezzang.sidepeek.projects.domain.file.File;
-
-public interface ProjectFileRepository extends JpaRepository<File, Long> {
-}
