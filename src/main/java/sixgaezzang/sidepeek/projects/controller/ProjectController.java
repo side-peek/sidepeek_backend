d4bba0c387b10a428191c920d99754f04706031e
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-02-20 15:20:57 +0900

    feat: 프로젝트 저장 컨트롤러 기능 구현

--- src/main/java/sixgaezzang/sidepeek/projects/controller/ProjectController.java
+++ src/main/java/sixgaezzang/sidepeek/projects/controller/ProjectController.java
@@ -1,14 +1,19 @@
 package sixgaezzang.sidepeek.projects.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import sixgaezzang.sidepeek.projects.dto.request.ProjectSaveRequest;
 import sixgaezzang.sidepeek.projects.dto.response.ProjectResponse;
 import sixgaezzang.sidepeek.projects.service.ProjectService;
 
@@ -19,6 +24,17 @@ import sixgaezzang.sidepeek.projects.service.ProjectService;
 public class ProjectController {
 
     private final ProjectService projectService;
+
+    @PostMapping
+    public ResponseEntity<Void> save(@RequestBody ProjectSaveRequest request) {
+        Long id = projectService.save(request);
+        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath()
+            .path("/projects/{id}")
+            .buildAndExpand(id).toUri();
+
+        return ResponseEntity.created(uri)
+            .build();
+    }
 
     @GetMapping("/{id}")
     @Operation(summary = "프로젝트 상세 조회")
