994f8b6f768df5ed31acc24bd2b68776a7c52bfb
uijin-j <uijin0531@gmail.com>
2024-02-21 19:01:09 +0900

    feat: LoginUserArgumentResolver 구현

---
+++ src/main/java/sixgaezzang/sidepeek/common/resolver/LoginUserArgumentResolver.java
@@ -0,0 +1,42 @@
+package sixgaezzang.sidepeek.common.resolver;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import sixgaezzang.sidepeek.common.annotation.Login;
+import sixgaezzang.sidepeek.common.exception.InvalidAuthenticationException;
+
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean hasLoginAnnotation = parameter.getParameterAnnotation(Login.class) != null;
+        boolean hasLongType = parameter.getParameterType()
+            .equals(Long.class);
+
+        return hasLoginAnnotation && hasLongType;
+    }
+
+    @Override
+    public Object resolveArgument(
+        MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest,
+        WebDataBinderFactory binderFactory
+    ) throws InvalidAuthenticationException {
+        Authentication authentication = SecurityContextHolder.getContext()
+            .getAuthentication();
+
+        if (authentication != null) {
+            String principal = (String) authentication.getPrincipal();
+            if (principal != null) {
+                return Long.parseLong(principal);
+            }
+        }
+
+        throw new InvalidAuthenticationException("로그인이 필요합니다.");
+    }
+
+}
