cdd4c1e9e7637eccd4252f7bba31dbdd63058b8b
uijin-j <uijin0531@gmail.com>
2024-02-22 04:13:46 +0900

    feat: refresh token 발급 기능 추가

--- src/main/java/sixgaezzang/sidepeek/auth/jwt/JWTManager.java
+++ src/main/java/sixgaezzang/sidepeek/auth/jwt/JWTManager.java
@@ -14,24 +14,29 @@ import sixgaezzang.sidepeek.config.properties.JWTProperties;
 @Component
 public class JWTManager {
 
-    private static final int SECONDS_PER_MINUTE = 60;
-    private static final long MILLISECONDS_PER_SECOND = 1000L;
+    private static final int MINUTE_PER_DAY = 24 * 60;
+    private static final long MILLISECONDS_PER_MINUTE = 60 * 1000L;
     public static final String USER_ID_CLAIM = "user_id";
 
     private final String issuer;
     private final SecretKey secretKey;
     private final long expiredAfter;
+    private final long refreshExpiredAfter;
 
     public JWTManager(JWTProperties jwtProperties) {
-        this.issuer = jwtProperties.issuer();
-        this.secretKey = Keys.hmacShaKeyFor(
-            jwtProperties.secretKey().getBytes(StandardCharsets.UTF_8));
-        this.expiredAfter =
-            jwtProperties.expiredAfter() * SECONDS_PER_MINUTE * MILLISECONDS_PER_SECOND;
+        issuer = jwtProperties.issuer();
+        secretKey = Keys.hmacShaKeyFor(jwtProperties.secretKey().getBytes(StandardCharsets.UTF_8));
+        expiredAfter = jwtProperties.expiredAfter() * MILLISECONDS_PER_MINUTE;
+        refreshExpiredAfter =
+            jwtProperties.refreshExpiredAfter() * MINUTE_PER_DAY * MILLISECONDS_PER_MINUTE;
     }
 
     public String generateAccessToken(long userId) {
         return generateToken(userId, expiredAfter);
+    }
+
+    public String generateRefreshToken(long userId) {
+        return generateToken(userId, refreshExpiredAfter);
     }
 
     public Long getUserId(String token) {
@@ -53,5 +58,15 @@ public class JWTManager {
             .claim(USER_ID_CLAIM, userId)
             .signWith(secretKey)
             .compact();
+    }
+
+    private Claims parseClaims(String token) {
+        try {
+            return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+        } catch (ExpiredJwtException e) {
+            throw new TokenValidationFailException("만료된 토큰입니다.");
+        } catch (Exception e) {
+            throw new TokenValidationFailException("유효하지 않은 토큰입니다.");
+        }
     }
 }
