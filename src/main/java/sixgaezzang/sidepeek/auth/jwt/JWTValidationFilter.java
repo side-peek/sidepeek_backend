e0574db7916ce0e6e0fcc6d4919314bea58a6e8e
uijin-j <uijin0531@gmail.com>
2024-02-21 18:40:15 +0900

    feat: JWT 인증을 위한 필터(JWTValidationFilter) 설정

---
+++ src/main/java/sixgaezzang/sidepeek/auth/jwt/JWTValidationFilter.java
@@ -0,0 +1,65 @@
+package sixgaezzang.sidepeek.auth.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import javax.crypto.SecretKey;
+import org.flywaydb.core.internal.util.StringUtils;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+import sixgaezzang.sidepeek.config.properties.JWTProperties;
+
+public class JWTValidationFilter extends OncePerRequestFilter {
+
+    public static final String JWT_HEADER = "Authorization";
+    public static final String JWT_PREFIX = "Bearer ";
+    public final String secretKey;
+
+    public JWTValidationFilter(JWTProperties jwtProperties) {
+        secretKey = jwtProperties.secretKey();
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+        FilterChain filterChain) throws ServletException, IOException {
+        String bearerToken = request.getHeader(JWT_HEADER);
+        String jwt = resolveToken(bearerToken);
+
+        if (null != jwt) {
+            try {
+                SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+                Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(jwt)
+                    .getBody();
+                String userId = String.valueOf(claims.get(JWTManager.USER_ID_CLAIM));
+                Authentication auth = new UsernamePasswordAuthenticationToken(userId, null);
+
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            } catch (Exception e) {
+                throw new BadCredentialsException("유효하지 않은 토큰입니다.");
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveToken(String bearerToken) {
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(JWT_PREFIX)) {
+            return bearerToken.substring(JWT_PREFIX.length());
+        }
+
+        return null;
+    }
+
+}
