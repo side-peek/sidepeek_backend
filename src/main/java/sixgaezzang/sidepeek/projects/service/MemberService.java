47b16cb333dfd17a8c818d0fd7f28302b7900eb7
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-02-22 11:25:19 +0900

    feat: 요청으로 들어온 회원 멤버가 존재하지 않는 회원인 경우 확인하는 로직 추가

--- src/main/java/sixgaezzang/sidepeek/projects/service/MemberService.java
+++ src/main/java/sixgaezzang/sidepeek/projects/service/MemberService.java
@@ -1,6 +1,8 @@
 package sixgaezzang.sidepeek.projects.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,23 +10,31 @@ import sixgaezzang.sidepeek.projects.domain.Project;
 import sixgaezzang.sidepeek.projects.domain.member.Member;
 import sixgaezzang.sidepeek.projects.dto.request.MemberSaveRequest;
 import sixgaezzang.sidepeek.projects.repository.MemberRepository;
+import sixgaezzang.sidepeek.users.repository.UserRepository;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MemberService {
 
+    private final UserRepository userRepository;
     private final MemberRepository memberRepository;
 
     @Transactional
     public void saveAll(Project project, List<MemberSaveRequest> memberSaveRequests) {
         List<Member> members = memberSaveRequests.stream().map(
-            member -> Member.builder()
-                .project(project)
-                .nickname(member.nickname())
-                .userId(member.userId())
-                .role(member.role())
-                .build()
+            member -> {
+                if (Objects.isNull(member.userId()) || userRepository.findById(member.userId()).isPresent()) {
+                    return Member.builder()
+                        .project(project)
+                        .nickname(member.nickname())
+                        .userId(member.userId())
+                        .role(member.role())
+                        .build();
+                }
+
+                throw new EntityNotFoundException("User Id에 해당하는 회원이 없습니다.");
+            }
         ).toList();
 
         memberRepository.saveAll(members);
