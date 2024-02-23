693b90b0b131eeb486fb69b70884079c28af1a2c
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-02-23 17:24:16 +0900

    feat: 공통 정규식 관리 클래스 생성

---
+++ src/main/java/sixgaezzang/sidepeek/common/util/Regex.java
@@ -0,0 +1,6 @@
+package sixgaezzang.sidepeek.common.util;
+
+public class Regex {
+    public static final String URL_REGEXP =
+        "^(https?)://([^:/\\s]+)(:([^/]*))?((/[^\\s/]+)*)?/?([^#\\s?]*)(\\?([^#\\s]*))?(#(\\w*))?$";
+}
