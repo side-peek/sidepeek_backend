3ee6502c710636a084bebcedd81e8a780df70a70
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-02-23 17:24:27 +0900

    feat: 프로젝트 상수 관리 클래스 생성

---
+++ src/main/java/sixgaezzang/sidepeek/projects/util/ProjectConstant.java
@@ -0,0 +1,14 @@
+package sixgaezzang.sidepeek.projects.util;
+
+public class ProjectConstant {
+    // Project
+    public static final int MAX_PROJECT_NAME_LENGTH = 300;
+    public static final int MAX_OVERVIEW_LENGTH = 1000;
+
+    // Member
+    public static final int MAX_ROLE_LENGTH = 15;
+
+    // ProjectSkill
+    public static final int MAX_CATEGORY_LENGTH = 50;
+
+}
