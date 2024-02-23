413a17382c6ac6f70d953c07ca8f03ce74710634
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-02-23 17:27:33 +0900

    feat: 프로젝트 도메인 request dto 유효성 검사 적용

--- src/main/java/sixgaezzang/sidepeek/projects/dto/request/ProjectSkillSaveRequest.java
+++ src/main/java/sixgaezzang/sidepeek/projects/dto/request/ProjectSkillSaveRequest.java
@@ -1,7 +1,21 @@
 package sixgaezzang.sidepeek.projects.dto.request;
 
+import static sixgaezzang.sidepeek.common.util.CommonConstant.MIN_ID;
+import static sixgaezzang.sidepeek.projects.util.ProjectConstant.MAX_CATEGORY_LENGTH;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record ProjectSkillSaveRequest(
+    @Min(value = MIN_ID, message = "스킬 id는 " + MIN_ID + "보다 작을 수 없습니다.")
+    @NotNull(message = "스킬 id를 입력해주세요.")
     Long skillId,
+
+    @Size(max = MAX_CATEGORY_LENGTH,
+        message = "스킬 카테고리는 " + MAX_CATEGORY_LENGTH + "자를 넘을 수 없습니다.")
+    @NotBlank(message = "스킬 카테고리를 입력해주세요.")
     String category
 ) {
 }
