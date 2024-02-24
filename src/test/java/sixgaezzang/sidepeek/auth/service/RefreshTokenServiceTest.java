3dae6267b1411f6cf2c5149fbb7339b3f97b2074
uijin-j <uijin0531@gmail.com>
2024-02-25 00:47:45 +0900

    test: 리프레시 토큰 조회 테스트 코드 작성

--- src/test/java/sixgaezzang/sidepeek/auth/service/RefreshTokenServiceTest.java
+++ src/test/java/sixgaezzang/sidepeek/auth/service/RefreshTokenServiceTest.java
@@ -16,6 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import sixgaezzang.sidepeek.auth.domain.RefreshToken;
 import sixgaezzang.sidepeek.auth.jwt.JWTManager;
+import sixgaezzang.sidepeek.auth.repository.RefreshTokenRepository;
 import sixgaezzang.sidepeek.common.exception.TokenValidationFailException;
 
 @SpringBootTest
@@ -27,6 +28,9 @@ class RefreshTokenServiceTest {
 
     @Autowired
     RefreshTokenService refreshTokenService;
+
+    @Autowired
+    RefreshTokenRepository refreshTokenRepository;
 
     @Autowired
     JWTManager jwtManager;
@@ -66,5 +70,44 @@ class RefreshTokenServiceTest {
             assertThatThrownBy(saven).isInstanceOf(TokenValidationFailException.class)
                 .hasMessage("유효하지 않은 토큰입니다.");
         }
+    }
+
+    @Nested
+    class 리프레시_토큰_조회_테스트 {
+
+        @Test
+        void 사용자ID를_통해_토큰_조회에_성공한다() {
+            // given
+            Long userId = faker.random().nextLong(Long.MAX_VALUE);
+            RefreshToken refreshToken = createRefreshToken(userId);
+
+            // when
+            RefreshToken actual = refreshTokenService.getById(userId);
+
+            // then
+            assertThat(actual).isNotNull();
+            assertThat(actual).extracting("userId", "refreshToken", "expiredAt")
+                .containsExactly(userId, refreshToken.refreshToken(), refreshToken.expiredAt());
+        }
+
+        @Test
+        void 사용자의_리프레시_토큰이_존재하지_않는_경우_조회에_실패한다() {
+            // given
+            Long userId = faker.random().nextLong(Long.MAX_VALUE);
+
+            // when
+            ThrowingCallable getById = () -> refreshTokenService.getById(userId);
+
+            // then
+            assertThatThrownBy(getById).isInstanceOf(TokenValidationFailException.class)
+                .hasMessage("유효하지 않은 토큰입니다.");
+        }
+    }
+
+    private RefreshToken createRefreshToken(Long userId) {
+        String refreshToken = jwtManager.generateRefreshToken(userId);
+        Long expirationAt = jwtManager.getExpiredAt(refreshToken);
+
+        return refreshTokenRepository.save(RefreshToken.from(userId, refreshToken, expirationAt));
     }
 }
