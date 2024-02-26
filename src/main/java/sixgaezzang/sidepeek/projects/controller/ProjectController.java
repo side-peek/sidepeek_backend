0c2b4f9849c78c578fe2ca945399eb0d1ec0bbe7
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-02-26 16:54:46 +0900

    refactor: 프로젝트 저장 시 저장된 프로젝트 정보 반환하는 것으로 수정

--- src/main/java/sixgaezzang/sidepeek/projects/controller/ProjectController.java
+++ src/main/java/sixgaezzang/sidepeek/projects/controller/ProjectController.java
@@ -27,16 +27,16 @@ public class ProjectController {
     private final ProjectService projectService;
 
     @PostMapping
-    public ResponseEntity<Void> save(
+    public ResponseEntity<ProjectResponse> save(
         @Valid @RequestBody ProjectSaveRequest request
     ) {
-        Long id = projectService.save(request);
+        ProjectResponse response = projectService.save(request);
         URI uri = ServletUriComponentsBuilder.fromCurrentContextPath()
             .path("/projects/{id}")
-            .buildAndExpand(id).toUri();
+            .buildAndExpand(response.id()).toUri();
 
         return ResponseEntity.created(uri)
-            .build();
+            .body(response);
     }
 
     @GetMapping("/{id}")
