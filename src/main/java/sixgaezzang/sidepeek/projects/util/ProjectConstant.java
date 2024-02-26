418b6cc7a3171b69b21c0d8317297d95678bb460
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-02-26 16:54:03 +0900

    refactor: 글자수 제한 수정 및 List 형태 갯수 제한 상수 설정

--- src/main/java/sixgaezzang/sidepeek/projects/util/ProjectConstant.java
+++ src/main/java/sixgaezzang/sidepeek/projects/util/ProjectConstant.java
@@ -2,13 +2,18 @@ package sixgaezzang.sidepeek.projects.util;
 
 public class ProjectConstant {
     // Project
-    public static final int MAX_PROJECT_NAME_LENGTH = 300;
-    public static final int MAX_OVERVIEW_LENGTH = 1000;
+    public static final int MAX_PROJECT_NAME_LENGTH = 50;
+    public static final int MAX_OVERVIEW_LENGTH = 300;
 
     // Member
+    public static final int MAX_MEMBER_COUNT = 10000;
     public static final int MAX_ROLE_LENGTH = 15;
 
     // ProjectSkill
+    public static final int MAX_PROJECT_SKILL_COUNT = 10000;
     public static final int MAX_CATEGORY_LENGTH = 50;
+
+    // OverviewImage(File)
+    public static final int MAX_OVERVIEW_IMAGE_COUNT = 6;
 
 }
