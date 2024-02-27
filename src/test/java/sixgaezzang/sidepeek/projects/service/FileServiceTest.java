4f66d9e468ed6cef5e63c25d776170f31ea9b8d8
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-02-27 17:08:11 +0900

    test: 레이아웃 이미지 저장 테스트 코드 작성

--- src/test/java/sixgaezzang/sidepeek/projects/service/FileServiceTest.java
+++ src/test/java/sixgaezzang/sidepeek/projects/service/FileServiceTest.java
@@ -3,10 +3,12 @@ package sixgaezzang.sidepeek.projects.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+import static sixgaezzang.sidepeek.projects.util.ProjectConstant.MAX_OVERVIEW_IMAGE_COUNT;
 
 import java.time.YearMonth;
 import java.util.ArrayList;
 import java.util.List;
+import net.datafaker.Faker;
 import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -17,21 +19,31 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 import sixgaezzang.sidepeek.projects.domain.Project;
 import sixgaezzang.sidepeek.projects.dto.response.OverviewImageSummary;
 import sixgaezzang.sidepeek.projects.repository.FileRepository;
 import sixgaezzang.sidepeek.projects.repository.ProjectRepository;
+import sixgaezzang.sidepeek.users.domain.Password;
+import sixgaezzang.sidepeek.users.domain.User;
+import sixgaezzang.sidepeek.users.repository.UserRepository;
 
 @SpringBootTest
 @Transactional
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class FileServiceTest {
+    static final Faker faker = new Faker();
 
     @Autowired
     FileService fileService;
+
     @Autowired
     FileRepository fileRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
     @Autowired
     ProjectRepository projectRepository;
 
@@ -39,17 +51,21 @@ class FileServiceTest {
     class 파일_저장_테스트 {
 
         static final int IMAGE_COUNT = 5;
+        static List<String> overLengthimageUrls;
         static List<String> imageUrls;
         Project project;
+        User user;
 
         @BeforeEach
         void setup() {
-            project = createProject();
-
-            imageUrls = new ArrayList<>();
-            for (int i = 0; i < IMAGE_COUNT; i++) {
-                imageUrls.add("https://sidepeek.image/image" + i);
+            overLengthimageUrls = new ArrayList<>();
+            for (int i = 1; i <= MAX_OVERVIEW_IMAGE_COUNT; i++) {
+                overLengthimageUrls.add("https://sidepeek.image/image" + i);
             }
+
+            user = createUser();
+            project = createProject(user);
+            imageUrls = overLengthimageUrls.subList(0, IMAGE_COUNT);
         }
 
         @Test
@@ -63,12 +79,22 @@ class FileServiceTest {
 
         @ParameterizedTest
         @NullAndEmptySource
-        void 빈_파일_목록_저장은_무시된다(List<String> emptyImageUrls) {
+        void 빈_파일_목록_저장은_무시되어_성공한다(List<String> emptyImageUrls) {
             // given, when
             List<OverviewImageSummary> savedImageUrls = fileService.saveAll(project, emptyImageUrls);
 
             // then
             assertThat(savedImageUrls).isNull();
+        }
+
+        @Test
+        void 목록_개수가_최대를_넘어서_파일_목록_저장에_실패한다() {
+            // given, when
+            ThrowableAssert.ThrowingCallable saveAll = () -> fileService.saveAll(project, overLengthimageUrls);
+
+            // then
+            assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(saveAll)
+                .withMessage("프로젝트 레이아웃 이미지 수는 " + MAX_OVERVIEW_IMAGE_COUNT + "개 미만이어야 합니다.");
         }
 
         @Test
@@ -84,23 +110,42 @@ class FileServiceTest {
                 .withMessage("프로젝트가 null 입니다.");
         }
 
-        private Project createProject() {
+        private User createUser() {
+            String email = faker.internet().emailAddress();
+            String password = faker.internet().password(8, 40, true, true, true);
+            String nickname = faker.internet().username();
+
+            User user = User.builder()
+                .email(email)
+                .password(new Password(password, new BCryptPasswordEncoder()))
+                .nickname(nickname)
+                .build();
+            return userRepository.save(user);
+        }
+
+        private Project createProject(User user) {
+            String name = faker.internet().domainName();
+            String subName = faker.internet().domainWord();
+            String overview = faker.lorem().sentence();
+            String thumbnailUrl = faker.internet().url();
+            String githubUrl = faker.internet().url();
+            YearMonth startDate = YearMonth.now();
+            YearMonth endDate = startDate.plusMonths(3);
+            String description = faker.lorem().sentences(10).toString();
+
             Project project = Project.builder()
-                .name("Project Name")
-                .subName("Project SubName")
-                .overview("Project Overview")
-                .ownerId(1L)
-                .githubUrl("https://github.com")
-                .description("Project description")
-                .thumbnailUrl("https://sidepeek.image/thumbnail")
-                .deployUrl("https://deploy.com")
-                .startDate(YearMonth.of(2023, 9))
-                .endDate(YearMonth.of(2024, 3))
-                .troubleshooting("Project TroubleShooting")
+                .name(name)
+                .subName(subName)
+                .overview(overview)
+                .thumbnailUrl(thumbnailUrl)
+                .githubUrl(githubUrl)
+                .startDate(startDate)
+                .endDate(endDate)
+                .ownerId(user.getId())
+                .description(description)
                 .build();
 
             return projectRepository.save(project);
         }
-
     }
 }
