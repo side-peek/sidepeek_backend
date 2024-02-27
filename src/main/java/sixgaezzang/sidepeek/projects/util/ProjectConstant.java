4f1a22ca266906dbb89eb20d23855c0584d41bd9
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-02-27 15:41:09 +0900

    refactor: 멤버와 기술스택 개수 상수 수정

--- src/main/java/sixgaezzang/sidepeek/projects/util/ProjectConstant.java
+++ src/main/java/sixgaezzang/sidepeek/projects/util/ProjectConstant.java
@@ -7,11 +7,11 @@ public class ProjectConstant {
     public static final String YEAR_MONTH_PATTERN = "yyyy-MM";
 
     // Member
-    public static final int MAX_MEMBER_COUNT = 10000;
+    public static final int MAX_MEMBER_COUNT = 20;
     public static final int MAX_ROLE_LENGTH = 15;
 
     // ProjectSkill
-    public static final int MAX_PROJECT_SKILL_COUNT = 10000;
+    public static final int MAX_PROJECT_SKILL_COUNT = 30;
     public static final int MAX_CATEGORY_LENGTH = 50;
 
     // OverviewImage(File)
