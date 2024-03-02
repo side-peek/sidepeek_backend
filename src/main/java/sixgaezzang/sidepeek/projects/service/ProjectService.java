54ffac80f2e90382c4cd261ab75dda2b115bcd11
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-02 17:30:06 +0900

    Revert "Revert "feat: 프로젝트 수정/삭제 스웨거 설명 추가""
    This reverts commit ee65fb4b84f82e1b1ab38f7426224fca5b75ecc6.

--- src/main/java/sixgaezzang/sidepeek/projects/service/ProjectService.java
+++ src/main/java/sixgaezzang/sidepeek/projects/service/ProjectService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sixgaezzang.sidepeek.projects.domain.Project;
 import sixgaezzang.sidepeek.projects.domain.file.FileType;
-import sixgaezzang.sidepeek.projects.dto.request.ProjectSaveRequest;
+import sixgaezzang.sidepeek.projects.dto.request.ProjectRequest;
 import sixgaezzang.sidepeek.projects.dto.response.MemberSummary;
 import sixgaezzang.sidepeek.projects.dto.response.OverviewImageSummary;
 import sixgaezzang.sidepeek.projects.dto.response.ProjectResponse;
@@ -26,19 +26,19 @@ public class ProjectService {
     private final FileService fileService;
 
     @Transactional
-    public ProjectResponse save(ProjectSaveRequest projectSaveRequest) {
-        // TODO: accessToken에서 userId 꺼내서 ownerId와 비교!!
+    public ProjectResponse save(ProjectRequest request) {
+        // TODO: loginId, ownerId와 비교!
 
-        Project project = projectSaveRequest.toEntity();
+        Project project = request.toEntity();
         projectRepository.save(project);
 
         // Required
-        List<ProjectSkillSummary> techStacks = projectSkillService.saveAll(project, projectSaveRequest.techStacks());
+        List<ProjectSkillSummary> techStacks = projectSkillService.saveAll(project, request.techStacks());
 
         // Option
-        List<MemberSummary> members = memberService.saveAll(project, projectSaveRequest.members());
+        List<MemberSummary> members = memberService.saveAll(project, request.members());
         List<OverviewImageSummary> overviewImages =
-            fileService.saveAll(project, projectSaveRequest.overviewImageUrls());
+            fileService.saveAll(project, request.overviewImageUrls());
 
         return ProjectResponse.from(project, overviewImages, techStacks, members);
     }
@@ -68,4 +68,14 @@ public class ProjectService {
         return ProjectResponse.from(project, overviewImages, techStacks, members);
     }
 
+    @Transactional
+    public ProjectResponse update(Long loginId, Long projectId, ProjectRequest request) {
+        // TODO: 작성자와 멤버만이 수정할 수 있다.
+        return null;
+    }
+
+    @Transactional
+    public void delete(Long loginId, Long projectId) {
+        // TODO: 작성자만이 삭제할 수 있다.
+    }
 }
