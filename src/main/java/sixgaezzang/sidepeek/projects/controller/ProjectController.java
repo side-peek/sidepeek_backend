ee65fb4b84f82e1b1ab38f7426224fca5b75ecc6
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-02 17:29:36 +0900

    Revert "feat: 프로젝트 수정/삭제 스웨거 설명 추가"
    This reverts commit 61ff10c88e3b6357525857cada5ef691f76805c1.

--- src/main/java/sixgaezzang/sidepeek/projects/controller/ProjectController.java
+++ src/main/java/sixgaezzang/sidepeek/projects/controller/ProjectController.java
@@ -1,24 +1,20 @@
 package sixgaezzang.sidepeek.projects.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import sixgaezzang.sidepeek.common.annotation.Login;
-import sixgaezzang.sidepeek.projects.dto.request.ProjectRequest;
+import sixgaezzang.sidepeek.projects.dto.request.ProjectSaveRequest;
 import sixgaezzang.sidepeek.projects.dto.response.ProjectResponse;
 import sixgaezzang.sidepeek.projects.service.ProjectService;
 
@@ -34,13 +30,7 @@ public class ProjectController {
     @Operation(summary = "프로젝트 생성")
     @ApiResponse(responseCode = "201", description = "프로젝트 생성 성공")
     public ResponseEntity<ProjectResponse> save(
-        @Schema(description = "로그인한 회원 식별자")
-        @Login
-        Long loginId,
-
-        @Valid
-        @RequestBody
-        ProjectRequest request
+        @Valid @RequestBody ProjectSaveRequest request
     ) {
         ProjectResponse response = projectService.save(request);
         URI uri = ServletUriComponentsBuilder.fromCurrentContextPath()
@@ -49,45 +39,6 @@ public class ProjectController {
 
         return ResponseEntity.created(uri)
             .body(response);
-    }
-
-    @PutMapping("/{id}")
-    @Operation(summary = "프로젝트 수정")
-    @ApiResponse(responseCode = "200", description = "프로젝트 수정 성공(프로젝트 작성자/회원 멤버만 가능)")
-    public ResponseEntity<ProjectResponse> update(
-        @Schema(description = "로그인한 회원 식별자")
-        @Login
-        Long loginId,
-
-        @Schema(description = "수정할 프로젝트 식별자")
-        @PathVariable(value = "id")
-        Long projectId,
-
-        @Valid
-        @RequestBody
-        ProjectRequest request
-    ) {
-        ProjectResponse response = projectService.update(loginId, projectId, request);
-
-        return ResponseEntity.ok(response);
-    }
-
-    @DeleteMapping("/{id}")
-    @Operation(summary = "프로젝트 삭제")
-    @ApiResponse(responseCode = "204", description = "프로젝트 삭제 성공(프로젝트 작성자만 가능)")
-    public ResponseEntity<Void> delete(
-        @Schema(description = "로그인한 회원 식별자")
-        @Login
-        Long loginId,
-
-        @Schema(description = "삭제할 프로젝트 식별자")
-        @PathVariable(value = "id")
-        Long projectId
-    ) {
-        projectService.delete(loginId, projectId);
-
-        return ResponseEntity.noContent()
-            .build();
     }
 
     @GetMapping("/{id}")
