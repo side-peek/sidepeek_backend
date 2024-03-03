c1382e818c4ff6c0f725de577d28e4f9101e5948
uijin-j <uijin0531@gmail.com>
2024-03-03 20:43:28 +0900

    feat: AuthProviderRepository QueryDSL 적용

--- src/main/java/sixgaezzang/sidepeek/auth/repository/AuthProviderRepository.java
+++ src/main/java/sixgaezzang/sidepeek/auth/repository/AuthProviderRepository.java
@@ -1,14 +1,9 @@
 package sixgaezzang.sidepeek.auth.repository;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import sixgaezzang.sidepeek.auth.domain.AuthProvider;
-import sixgaezzang.sidepeek.auth.domain.ProviderType;
 
-public interface AuthProviderRepository extends JpaRepository<AuthProvider, Long> {
+public interface AuthProviderRepository extends JpaRepository<AuthProvider, Long>,
+    AuthProviderRepositoryCustom {
 
-    @Query("SELECT a FROM AuthProvider a JOIN FETCH a.user WHERE a.providerType = :providerType AND a.providerId = :providerId")
-    Optional<AuthProvider> findByProviderTypeAndProviderId(ProviderType providerType,
-        String providerId);
 }
