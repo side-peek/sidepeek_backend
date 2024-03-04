d696b30361d03a138990237490f6a21a7799d092
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-04 17:19:05 +0900

    feat: User Id에 해당하는 유저가 존재하지 않을 경우 에러 메시지 정의

--- src/main/java/sixgaezzang/sidepeek/users/exception/message/UserErrorMessage.java
+++ src/main/java/sixgaezzang/sidepeek/users/exception/message/UserErrorMessage.java
@@ -7,6 +7,7 @@ import static sixgaezzang.sidepeek.users.util.UserConstant.MAX_JOB_LENGTH;
 import static sixgaezzang.sidepeek.users.util.UserConstant.MAX_NICKNAME_LENGTH;
 
 public class UserErrorMessage {
+    public static final String USER_NOT_EXISTING = "User Id에 해당하는 회원이 없습니다.";
     public static final String NICKNAME_IS_NULL = "닉네임을 입력해주세요.";
     public static final String NICKNAME_OVER_MAX_LENGTH = "닉네임은 " + MAX_NICKNAME_LENGTH + "자 이하여야 합니다.";
     public static final String INTRODUCTION_OVER_MAX_LENGTH = "소개글은 " + MAX_INTRODUCTION_LENGTH + "자 이하여야 합니다.";
