761f2a3c228286d101631a5eaad74ce6f207505a
EomYerim <djadpfa1002@naver.com>
2024-03-05 05:48:36 +0900

    feat: project 전체 조회 api 엔드 포인트 추가 및 swagger 설정

--- src/main/java/sixgaezzang/sidepeek/projects/controller/ProjectController.java
+++ src/main/java/sixgaezzang/sidepeek/projects/controller/ProjectController.java
@@ -1,9 +1,12 @@
 package sixgaezzang.sidepeek.projects.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.net.URI;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +14,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import sixgaezzang.sidepeek.projects.dto.request.ProjectSaveRequest;
+import sixgaezzang.sidepeek.projects.dto.response.ProjectListResponse;
 import sixgaezzang.sidepeek.projects.dto.response.ProjectResponse;
 import sixgaezzang.sidepeek.projects.service.ProjectService;
 
@@ -47,4 +52,23 @@ public class ProjectController {
         return ResponseEntity.ok(response);
     }
 
+    @GetMapping
+    @Operation(summary = "프로젝트 전체 조회")
+    @ApiResponse(responseCode = "200", description = "프로젝트 전체 조회 성공")
+    @Parameters({
+        @Parameter(name = "userId", description = "로그인한 사용자의 ID"),
+        @Parameter(name = "sort", description = "정렬 조건 [ createdAt(default), view, like ]"),
+        @Parameter(name = "isReleased", description = "출시서비스만 보기")
+    })
+    public ResponseEntity<List<ProjectListResponse>> getAll(
+        @RequestParam(required = false) Long userId,
+        @RequestParam(required = false) String sort,
+        @RequestParam(required = false, defaultValue = "false") boolean isReleased
+    ) {
+        sort = (sort == null) ? "createdAt" : sort;
+        List<ProjectListResponse> responses = projectService.findAll(userId, sort, isReleased);
+        //TODO: @Login 어노테이션에서 requried=false를 활용할 순 없을까? (로그인한 사용자가 좋아요한 프로젝트를 확인하기 위해)
+
+        return ResponseEntity.ok(responses);
+    }
 }
