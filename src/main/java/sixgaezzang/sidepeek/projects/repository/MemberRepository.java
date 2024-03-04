e80e235430b6b73edff55167bf5fa45d45db3243
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-04 16:15:10 +0900

    feat: MemberService saveAll에 필요한 인터페이스 메서드 정의

--- src/main/java/sixgaezzang/sidepeek/projects/repository/MemberRepository.java
+++ src/main/java/sixgaezzang/sidepeek/projects/repository/MemberRepository.java
@@ -1,8 +1,13 @@
 package sixgaezzang.sidepeek.projects.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import sixgaezzang.sidepeek.projects.domain.Project;
 import sixgaezzang.sidepeek.projects.domain.member.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+
+    boolean existsByProject(Project project);
+
+    void deleteAllByProject(Project project);
 
 }
