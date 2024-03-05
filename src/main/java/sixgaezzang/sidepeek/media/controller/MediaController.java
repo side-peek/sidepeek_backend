3d9de2065e8723e85651fedb286752dcc6bfef44
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-05 23:40:23 +0900

    fix: checkstyle 에러 해결(120자)

--- src/main/java/sixgaezzang/sidepeek/media/controller/MediaController.java
+++ src/main/java/sixgaezzang/sidepeek/media/controller/MediaController.java
@@ -30,7 +30,8 @@ public class MediaController {
     @Operation(summary = "파일 업로드")
     @ApiResponse(responseCode = "200", description = "파일 업로드 성공")
     @ApiResponse(responseCode = "400",
-        description = "요청 파일이 잘못된 경우(파일이 없거나 비어있는 경우. 파일 형식이 이미지/비디오 형식이 아닌 경우. 최대 용량(100MB)를 넘은 경우.)", content = @Content)
+        description = "요청이 잘못된 경우(파일이 없거나 비어있는 경우. 이미지/비디오 형식이 아닌 경우. 최대 용량(100MB)를 넘은 경우.)",
+        content = @Content)
     @ApiResponse(responseCode = "401", description = "로그인을 하지 않은 경우", content = @Content)
     @ApiResponse(responseCode = "5XX", description = "파일 업로드 중 그 외 예외가 발생한 경우", content = @Content)
     public ResponseEntity<MediaUploadResponse> uploadFile(
