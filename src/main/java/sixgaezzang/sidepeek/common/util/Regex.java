034a05d0669ff1970f278b1ca9f63ef835f5e4f0
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-05 13:31:01 +0900

    refactor: 정규식 상수 private 반영

--- src/main/java/sixgaezzang/sidepeek/common/util/Regex.java
+++ src/main/java/sixgaezzang/sidepeek/common/util/Regex.java
@@ -1,5 +1,9 @@
 package sixgaezzang.sidepeek.common.util;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Regex {
     public static final String URL_REGEXP =
         "^(https?)://([^:/\\s]+)(:([^/]*))?((/[^\\s/]+)*)?/?([^#\\s?]*)(\\?([^#\\s]*))?(#(\\w*))?$";
