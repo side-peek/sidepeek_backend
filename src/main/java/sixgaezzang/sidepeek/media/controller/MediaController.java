1f052138056556262023667ab0815da46ad7cf40
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-05 17:54:13 +0900

    feat: 이미지 업로드 컨트롤러 스웨거 적용 및 로그인 id 파라미터 추가

--- src/main/java/sixgaezzang/sidepeek/media/controller/MediaController.java
+++ src/main/java/sixgaezzang/sidepeek/media/controller/MediaController.java
@@ -1,7 +1,10 @@
 package sixgaezzang.sidepeek.media.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,11 +13,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import sixgaezzang.sidepeek.common.annotation.Login;
 import sixgaezzang.sidepeek.media.dto.response.MediaUploadResponse;
 import sixgaezzang.sidepeek.media.service.MediaService;
 
-@RequestMapping("/files")
 @RestController
+@RequestMapping("/files")
+@Tag(name = "Media", description = "Media(Image Upload) API")
 @RequiredArgsConstructor
 public class MediaController {
 
@@ -24,11 +29,16 @@ public class MediaController {
     @Operation(summary = "파일 업로드")
     @ApiResponse(responseCode = "200", description = "파일 업로드 성공")
     public ResponseEntity<MediaUploadResponse> uploadFile(
+        @Parameter(name = "loginId", description = "로그인한 회원 식별자", in = ParameterIn.HEADER)
+        @Login
+        Long loginId,
+
+        @Parameter(name = "file", description = "업로드할 이미지 파일")
         @RequestParam
         MultipartFile file
     ) {
         return ResponseEntity.ok()
-            .body(mediaService.uploadFile(file));
+            .body(mediaService.uploadFile(loginId, file));
     }
 
 }
