df4449d1617e4947d96e174bcc88ecf0a169156e
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-05 18:16:48 +0900

    feat: MediaController uploadFile에 예외 응답 스웨거 적용

--- src/main/java/sixgaezzang/sidepeek/media/controller/MediaController.java
+++ src/main/java/sixgaezzang/sidepeek/media/controller/MediaController.java
@@ -3,6 +3,7 @@ package sixgaezzang.sidepeek.media.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -28,6 +29,10 @@ public class MediaController {
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "파일 업로드")
     @ApiResponse(responseCode = "200", description = "파일 업로드 성공")
+    @ApiResponse(responseCode = "400",
+        description = "요청 파일이 잘못된 경우(파일이 없거나 비어있는 경우. 파일 형식이 이미지/비디오 형식이 아닌 경우. 최대 용량(100MB)를 넘은 경우.)", content = @Content)
+    @ApiResponse(responseCode = "401", description = "로그인을 하지 않은 경우", content = @Content)
+    @ApiResponse(responseCode = "5XX", description = "파일 업로드 중 그 외 예외가 발생한 경우", content = @Content)
     public ResponseEntity<MediaUploadResponse> uploadFile(
         @Parameter(name = "loginId", description = "로그인한 회원 식별자", in = ParameterIn.HEADER)
         @Login
