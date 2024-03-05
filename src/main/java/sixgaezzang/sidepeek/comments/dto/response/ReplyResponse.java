37b5c5c9c09e65b4a039b0f6134fcd4476fc319f
EomYerim <djadpfa1002@naver.com>
2024-03-06 01:55:01 +0900

    feat: comment 대댓글 응답 dto 생성

---
+++ src/main/java/sixgaezzang/sidepeek/comments/dto/response/ReplyResponse.java
@@ -0,0 +1,51 @@
+package sixgaezzang.sidepeek.comments.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import sixgaezzang.sidepeek.comments.domain.Comment;
+import sixgaezzang.sidepeek.users.domain.User;
+import sixgaezzang.sidepeek.users.dto.response.UserSummary;
+
+@Builder
+@Schema(description = "대댓글 응답 정보")
+public record ReplyResponse(
+    @Schema(description = "댓글 식별자", example = "3")
+    Long id,
+
+    @Schema(description = "상위 댓글 식별자(상위 댓글이라면 null)", example = "1")
+    Long parentId,
+
+    @Schema(description = "대댓글 작성자 정보(익명 댓글은 null)")
+    UserSummary user,
+
+    @Schema(description = "대댓글 작성자와 로그인 사용자 일치 여부")
+    boolean isOwner,
+
+    @Schema(description = "익명 대댓글 여부")
+    boolean isAnonymous,
+
+    @Schema(description = "댓글 내용", example = "좋게 봐주셔서 감사합니다!")
+    String content,
+
+    @Schema(description = "대댓글 생성 시각", example = "2024-03-05 20:17:00", type = "string")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    LocalDateTime createdAt
+) {
+
+    public static ReplyResponse from(Comment comment, boolean isOwner) {
+        User user = comment.getUser();
+
+        return ReplyResponse.builder()
+            .id(comment.getId())
+            .parentId(comment.getParent().getId())
+            .user(UserSummary.from(user))
+            .isOwner(isOwner)
+            .isAnonymous(comment.isAnonymous())
+            .content(comment.getContent())
+            .createdAt(comment.getCreatedAt())
+            .build();
+    }
+
+}
