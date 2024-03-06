00444557d91e7e4ee7e4c9b9bc2b9c8f5abed916
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-06 14:17:31 +0900

    feat: UserValidator에 validateUserId 구현

--- src/main/java/sixgaezzang/sidepeek/users/util/validation/UserValidator.java
+++ src/main/java/sixgaezzang/sidepeek/users/util/validation/UserValidator.java
@@ -4,15 +4,23 @@ import static sixgaezzang.sidepeek.common.util.ValidationUtils.validateMaxLength
 import static sixgaezzang.sidepeek.common.util.ValidationUtils.validateNotBlank;
 import static sixgaezzang.sidepeek.users.exception.message.UserErrorMessage.NICKNAME_IS_NULL;
 import static sixgaezzang.sidepeek.users.exception.message.UserErrorMessage.NICKNAME_OVER_MAX_LENGTH;
+import static sixgaezzang.sidepeek.users.exception.message.UserErrorMessage.USER_ID_IS_NULL;
 import static sixgaezzang.sidepeek.users.util.UserConstant.MAX_NICKNAME_LENGTH;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.springframework.util.Assert;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserValidator {
+
+    public static void validateUserId(Long id) {
+        Assert.notNull(id, USER_ID_IS_NULL);
+    }
+
     public static void validateNickname(String nickname) {
         validateNotBlank(nickname, NICKNAME_IS_NULL);
         validateMaxLength(nickname, MAX_NICKNAME_LENGTH, NICKNAME_OVER_MAX_LENGTH);
     }
+
 }
