20e3ced7b1f5c80f8c706c90992098f041fd1bb2
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-06 11:32:40 +0900

    refactor: SaveMemberRequest에 nickname @NotBlank 설정 및 Swagger 설명 수정

--- src/main/java/sixgaezzang/sidepeek/projects/dto/request/SaveMemberRequest.java
+++ src/main/java/sixgaezzang/sidepeek/projects/dto/request/SaveMemberRequest.java
@@ -1,9 +1,11 @@
 package sixgaezzang.sidepeek.projects.dto.request;
 
 import static sixgaezzang.sidepeek.common.util.CommonConstant.MIN_ID;
-import static sixgaezzang.sidepeek.projects.exception.message.MemberErrorMessage.NON_FELLOW_MEMBER_NICKNAME_OVER_MAX_LENGTH;
+import static sixgaezzang.sidepeek.projects.exception.message.MemberErrorMessage.ROLE_IS_NULL;
 import static sixgaezzang.sidepeek.projects.exception.message.MemberErrorMessage.ROLE_OVER_MAX_LENGTH;
 import static sixgaezzang.sidepeek.projects.util.ProjectConstant.MAX_ROLE_LENGTH;
+import static sixgaezzang.sidepeek.users.exception.message.UserErrorMessage.NICKNAME_IS_NULL;
+import static sixgaezzang.sidepeek.users.exception.message.UserErrorMessage.NICKNAME_OVER_MAX_LENGTH;
 import static sixgaezzang.sidepeek.users.util.UserConstant.MAX_NICKNAME_LENGTH;
 
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -17,13 +19,14 @@ public record SaveMemberRequest(
     @Min(value = MIN_ID, message = "멤버 회원 id는 " + MIN_ID + "보다 작을 수 없습니다.")
     Long userId,
 
-    @Schema(description = "비회원 멤버 닉네임(회원 멤버이면 null)", example = " ")
-    @Size(max = MAX_NICKNAME_LENGTH, message = NON_FELLOW_MEMBER_NICKNAME_OVER_MAX_LENGTH)
+    @Schema(description = "멤버 닉네임(회원도 설정 가능)", example = "Jinny")
+    @Size(max = MAX_NICKNAME_LENGTH, message = NICKNAME_OVER_MAX_LENGTH)
+    @NotBlank(message = NICKNAME_IS_NULL)
     String nickname,
 
     @Schema(description = "멤버 역할", example = "백엔드")
     @Size(max = MAX_ROLE_LENGTH, message = ROLE_OVER_MAX_LENGTH)
-    @NotBlank(message = "멤버 역할을 입력해주세요.")
+    @NotBlank(message = ROLE_IS_NULL)
     String role
 ) {
 }
