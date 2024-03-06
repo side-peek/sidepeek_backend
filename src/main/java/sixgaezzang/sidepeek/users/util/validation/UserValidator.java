bfac507ef03153afe52cdf491b3ca218d6ac6ea9
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-06 15:06:40 +0900

    feat: UserValidator에 User 수정 관련 유효성 검사 메서드 구현

--- src/main/java/sixgaezzang/sidepeek/users/util/validation/UserValidator.java
+++ src/main/java/sixgaezzang/sidepeek/users/util/validation/UserValidator.java
@@ -1,15 +1,31 @@
 package sixgaezzang.sidepeek.users.util.validation;
 
+import static sixgaezzang.sidepeek.common.exception.message.CommonErrorMessage.GITHUB_URL_IS_INVALID;
+import static sixgaezzang.sidepeek.common.exception.message.CommonErrorMessage.GITHUB_URL_OVER_MAX_LENGTH;
+import static sixgaezzang.sidepeek.common.util.CommonConstant.MAX_TEXT_LENGTH;
 import static sixgaezzang.sidepeek.common.util.ValidationUtils.validateMaxLength;
 import static sixgaezzang.sidepeek.common.util.ValidationUtils.validateNotBlank;
+import static sixgaezzang.sidepeek.users.exception.message.UserErrorMessage.BLOG_URL_IS_INVALID;
+import static sixgaezzang.sidepeek.users.exception.message.UserErrorMessage.BLOG_URL_OVER_MAX_LENGTH;
+import static sixgaezzang.sidepeek.users.exception.message.UserErrorMessage.CAREER_IS_INVALID;
+import static sixgaezzang.sidepeek.users.exception.message.UserErrorMessage.INTRODUCTION_OVER_MAX_LENGTH;
+import static sixgaezzang.sidepeek.users.exception.message.UserErrorMessage.JOB_IS_INVALID;
 import static sixgaezzang.sidepeek.users.exception.message.UserErrorMessage.NICKNAME_IS_NULL;
 import static sixgaezzang.sidepeek.users.exception.message.UserErrorMessage.NICKNAME_OVER_MAX_LENGTH;
+import static sixgaezzang.sidepeek.users.exception.message.UserErrorMessage.PROFILE_IMAGE_URL_IS_INVALID;
+import static sixgaezzang.sidepeek.users.exception.message.UserErrorMessage.PROFILE_IMAGE_URL_OVER_MAX_LENGTH;
 import static sixgaezzang.sidepeek.users.exception.message.UserErrorMessage.USER_ID_IS_NULL;
+import static sixgaezzang.sidepeek.users.util.UserConstant.MAX_INTRODUCTION_LENGTH;
 import static sixgaezzang.sidepeek.users.util.UserConstant.MAX_NICKNAME_LENGTH;
 
+import java.util.Arrays;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.springframework.util.Assert;
+import sixgaezzang.sidepeek.common.util.ValidationUtils;
+import sixgaezzang.sidepeek.users.domain.Career;
+import sixgaezzang.sidepeek.users.domain.Job;
+import sixgaezzang.sidepeek.users.domain.Password;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserValidator {
@@ -21,6 +37,43 @@ public class UserValidator {
     public static void validateNickname(String nickname) {
         validateNotBlank(nickname, NICKNAME_IS_NULL);
         validateMaxLength(nickname, MAX_NICKNAME_LENGTH, NICKNAME_OVER_MAX_LENGTH);
+    }
+
+    public static void validateIntroduction(String introduction) {
+        validateMaxLength(introduction, MAX_INTRODUCTION_LENGTH, INTRODUCTION_OVER_MAX_LENGTH);
+    }
+
+    public static void validateJob(String jobName) {
+        Arrays.stream(Job.values())
+            .filter(job -> jobName.equals(job.getName()))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException(JOB_IS_INVALID));
+    }
+
+    public static void validateCareer(String careerDescription) {
+        Arrays.stream(Career.values())
+            .filter(career -> careerDescription.equals(career.getDescription()))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException(CAREER_IS_INVALID));
+    }
+
+    public static void validateProfileImageUrl(String profileImageUrl) {
+        validateMaxLength(profileImageUrl, MAX_TEXT_LENGTH, PROFILE_IMAGE_URL_OVER_MAX_LENGTH);
+        ValidationUtils.validateURI(profileImageUrl, PROFILE_IMAGE_URL_IS_INVALID);
+    }
+
+    public static void validateGithubUrl(String githubUrl) {
+        validateMaxLength(githubUrl, MAX_TEXT_LENGTH, GITHUB_URL_OVER_MAX_LENGTH);
+        ValidationUtils.validateURI(githubUrl, GITHUB_URL_IS_INVALID);
+    }
+
+    public static void validateBlogUrl(String blogUrl) {
+        validateMaxLength(blogUrl, MAX_TEXT_LENGTH, BLOG_URL_OVER_MAX_LENGTH);
+        ValidationUtils.validateURI(blogUrl, BLOG_URL_IS_INVALID);
+    }
+
+    public static void validatePassword(Password password) {
+        // TODO: 비밀번호 유효성 검사
     }
 
 }
