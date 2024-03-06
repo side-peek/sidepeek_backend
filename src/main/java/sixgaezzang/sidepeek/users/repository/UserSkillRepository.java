9bb10dec63cb4a99d969e7501594d938cc8a9677
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-06 13:17:36 +0900

    feat: UserSkillRepository 인터페이스 구현

---
+++ src/main/java/sixgaezzang/sidepeek/users/repository/UserSkillRepository.java
@@ -0,0 +1,7 @@
+package sixgaezzang.sidepeek.users.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sixgaezzang.sidepeek.users.domain.UserSkill;
+
+public interface UserSkillRepository extends JpaRepository<UserSkill, Long> {
+}
