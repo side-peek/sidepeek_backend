5221b7c668a5003c5c2c2e69f75f646c3c62011e
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-06 13:19:24 +0900

    rename: UserSkillRepository userskill 패키지로 이동

--- src/main/java/sixgaezzang/sidepeek/users/repository/UserSkillRepository.java
+++
@@ -1,7 +0,0 @@
-package sixgaezzang.sidepeek.users.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sixgaezzang.sidepeek.users.domain.UserSkill;
-
-public interface UserSkillRepository extends JpaRepository<UserSkill, Long> {
-}
---
+++ src/main/java/sixgaezzang/sidepeek/users/repository/userskill/UserSkillRepository.java
@@ -0,0 +1,7 @@
+package sixgaezzang.sidepeek.users.repository.userskill;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sixgaezzang.sidepeek.users.domain.UserSkill;
+
+public interface UserSkillRepository extends JpaRepository<UserSkill, Long>  {
+}
