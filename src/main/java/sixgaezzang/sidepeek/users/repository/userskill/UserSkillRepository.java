ca72887a09ec84efca6f98e1fba579943263b4c3
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-06 13:21:54 +0900

    feat: UserSkillRepository가 UserSkillRepositoryCustom 상속하도록 구현

--- src/main/java/sixgaezzang/sidepeek/users/repository/userskill/UserSkillRepository.java
+++ src/main/java/sixgaezzang/sidepeek/users/repository/userskill/UserSkillRepository.java
@@ -3,5 +3,5 @@ package sixgaezzang.sidepeek.users.repository.userskill;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sixgaezzang.sidepeek.users.domain.UserSkill;
 
-public interface UserSkillRepository extends JpaRepository<UserSkill, Long>  {
+public interface UserSkillRepository extends JpaRepository<UserSkill, Long>, UserSkillRepositoryCustom {
 }
