45c3331e1d76e2f236d4395a39ccbd16bf9d168a
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-06 13:46:46 +0900

    feat: UserSkillService에서 findAllByUser 정의

--- src/main/java/sixgaezzang/sidepeek/users/service/UserSkillService.java
+++ src/main/java/sixgaezzang/sidepeek/users/service/UserSkillService.java
@@ -1,9 +1,11 @@
 package sixgaezzang.sidepeek.users.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import sixgaezzang.sidepeek.users.repository.UserRepository;
+import sixgaezzang.sidepeek.users.domain.User;
+import sixgaezzang.sidepeek.users.dto.response.UserSkillSummary;
 import sixgaezzang.sidepeek.users.repository.userskill.UserSkillRepository;
 
 @Service
@@ -11,7 +13,13 @@ import sixgaezzang.sidepeek.users.repository.userskill.UserSkillRepository;
 @RequiredArgsConstructor
 public class UserSkillService {
 
-    private final UserRepository userRepository;
     private final UserSkillRepository userSkillRepository;
+
+    public List<UserSkillSummary> findAllByUser(User user) {
+        return userSkillRepository.findAllByUser(user)
+            .stream()
+            .map(UserSkillSummary::from)
+            .toList();
+    }
 
 }
