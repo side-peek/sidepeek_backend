d741f4de1eac7ef7627f5723be347430ff272bb3
EomYerim <djadpfa1002@naver.com>
2024-03-06 13:58:11 +0900

    feat: comment repository 댓글 조회 관련 querydsl 적용

--- src/main/java/sixgaezzang/sidepeek/comments/repository/CommentRepository.java
+++ src/main/java/sixgaezzang/sidepeek/comments/repository/CommentRepository.java
@@ -1,14 +1,8 @@
 package sixgaezzang.sidepeek.comments.repository;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sixgaezzang.sidepeek.comments.domain.Comment;
-import sixgaezzang.sidepeek.projects.domain.Project;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-    List<Comment> findAllByProject(Project project);
-
-    List<Comment> findAllByParent(Comment comment);
+public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom {
 
 }
