5ad15dff5aea935211fe19ca596cb7d8e8767cba
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-06 17:58:49 +0900

    style: 줄바꿈 수정

--- src/main/java/sixgaezzang/sidepeek/projects/repository/MemberRepository.java
+++ src/main/java/sixgaezzang/sidepeek/projects/repository/MemberRepository.java
@@ -6,6 +6,7 @@ import sixgaezzang.sidepeek.projects.domain.Project;
 import sixgaezzang.sidepeek.projects.domain.member.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+
     List<Member> findAllByProject(Project project);
 
     boolean existsByProject(Project project);
