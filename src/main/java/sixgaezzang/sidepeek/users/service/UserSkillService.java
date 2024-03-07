3e2d49c6585324a90048425b7a166ce50f728648
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-07 17:58:49 +0900

    fix: UserSkillService saveAll 로직 에러 수정

--- src/main/java/sixgaezzang/sidepeek/users/service/UserSkillService.java
+++ src/main/java/sixgaezzang/sidepeek/users/service/UserSkillService.java
@@ -1,12 +1,16 @@
 package sixgaezzang.sidepeek.users.service;
 
 import static sixgaezzang.sidepeek.skill.exception.message.SkillErrorMessage.SKILL_NOT_EXISTING;
+import static sixgaezzang.sidepeek.users.util.validation.UserSkillValidator.validateUserTechStacks;
+import static sixgaezzang.sidepeek.users.util.validation.UserValidator.validateUser;
 
 import jakarta.persistence.EntityNotFoundException;
+import java.util.Collections;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import sixgaezzang.sidepeek.common.util.ValidationUtils;
 import sixgaezzang.sidepeek.skill.domain.Skill;
 import sixgaezzang.sidepeek.skill.repository.SkillRepository;
 import sixgaezzang.sidepeek.users.domain.User;
@@ -14,8 +18,6 @@ import sixgaezzang.sidepeek.users.domain.UserSkill;
 import sixgaezzang.sidepeek.users.dto.request.UpdateUserSkillRequest;
 import sixgaezzang.sidepeek.users.dto.response.UserSkillSummary;
 import sixgaezzang.sidepeek.users.repository.userskill.UserSkillRepository;
-import sixgaezzang.sidepeek.users.util.validation.UserSkillValidator;
-import sixgaezzang.sidepeek.users.util.validation.UserValidator;
 
 @Service
 @Transactional(readOnly = true)
@@ -26,7 +28,7 @@ public class UserSkillService {
     private final SkillRepository skillRepository;
 
     public List<UserSkillSummary> findAllByUser(User user) {
-        UserValidator.validateUser(user);
+        validateUser(user);
 
         return userSkillRepository.findAllByUser(user)
             .stream()
@@ -35,13 +37,16 @@ public class UserSkillService {
     }
 
     public List<UserSkillSummary> saveAll(User user, List<UpdateUserSkillRequest> techStacks) {
-        UserValidator.validateUser(user);
-        UserSkillValidator.validateUserTechStacks(techStacks);
-
+        validateUser(user);
         if (userSkillRepository.existsByUser(user)) {
             userSkillRepository.deleteAllByUser(user);
         }
 
+        if (ValidationUtils.isNotNullOrEmpty(techStacks)) {
+            return Collections.emptyList();
+        }
+
+        validateUserTechStacks(techStacks);
         List<UserSkill> skills = convertAllToEntity(user, techStacks);
         userSkillRepository.saveAll(skills);
 
@@ -51,13 +56,13 @@ public class UserSkillService {
     }
 
     private List<UserSkill> convertAllToEntity(User user, List<UpdateUserSkillRequest> techStacks) {
-        return techStacks.stream().map(
-            techStack -> {
+        return techStacks.stream()
+            .map(techStack -> {
                 Skill skill = skillRepository.findById(techStack.skillId())
                     .orElseThrow(() -> new EntityNotFoundException(SKILL_NOT_EXISTING));
 
                 return techStack.toEntity(user, skill);
-            }
-        ).toList();
+            })
+            .toList();
     }
 }
