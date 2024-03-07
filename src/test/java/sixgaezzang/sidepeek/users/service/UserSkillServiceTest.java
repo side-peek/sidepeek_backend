2c67ea698bf4b0596f7dc72dc3b4aee54412e6b5
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-07 14:49:46 +0900

    feat: UserSkillServiceTest 테스트 메서드 정의

--- src/test/java/sixgaezzang/sidepeek/users/service/UserSkillServiceTest.java
+++ src/test/java/sixgaezzang/sidepeek/users/service/UserSkillServiceTest.java
@@ -1,16 +1,107 @@
 package sixgaezzang.sidepeek.users.service;
 
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
 class UserSkillServiceTest {
-    // TODO: UserSkillServiceTest 작성 필요
+
     @Nested
-    class 사용자_기술_스택_조회_테스트 {
+    class 사용자_기술_스택_목록_조회_테스트 {
+        // TODO: 사용자_기술_스택_목록_조회_테스트 작성 필요
+        @Test
+        void 사용자_기술_스택_목록_조회에_성공한다() {
+            // given
+
+            // when
+
+            // then
+
+        }
+
+        @Test
+        void 사용자가_null_이라서_사용자_기술_스택_목록_조회에_실패한다() {
+            // given
+
+            // when
+
+            // then
+
+        }
 
     }
 
     @Nested
-    class 사용자_기술_스택_수정_테스트 {
+    class 사용자_기술_스택_목록_수정_테스트 {
+        // TODO: 사용자_기술_스택_목록_수정_테스트 작성 필요
+        @Test
+        void 사용자_기술_스택_목록_수정에_성공한다() {
+            // given
+
+            // when
+
+            // then
+
+        }
+
+        @Test
+        void 사용자가_null_이라서_사용자_기술_스택_목록_수정에_실패한다() {
+            // given
+
+            // when
+
+            // then
+
+        }
+
+        @Test
+        void 목록_개수가_최대를_넘어서_사용자_기술_스택_목록_수정에_실패한다() {
+            // given
+
+            // when
+
+            // then
+
+        }
+
+        @Test
+        void 유효하지_않은_기술_스택_카테고리로_사용자_기술_스택_목록_수정에_실패한다() {
+            // given
+
+            // when
+
+            // then
+
+        }
+
+        @Test
+        void 존재하지_않는_기술_스택_Id로_사용자_기술_스택_목록_수정에_실패한다() {
+            // given
+
+            // when
+
+            // then
+
+        }
+
+        @Test
+        void 기술_스택_Id가_누락되어_사용자_기술_스택_목록_저장에_실패한다() {
+            // given
+
+            // when
+
+            // then
+
+        }
+
+        @Test
+        void 같은_카테고리_내에_중복된_기술_스택으로_사용자_기술_스택_목록_수정에_실패한다() {
+            // given
+
+            // when
+
+            // then
+
+        }
 
     }
 
