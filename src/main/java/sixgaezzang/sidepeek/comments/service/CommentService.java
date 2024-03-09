481f960e9763796f983342b00ec443267e7d2d1e
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-10 02:41:59 +0900

    feat: CommentService 생성, 수정 기능 구현

--- src/main/java/sixgaezzang/sidepeek/comments/service/CommentService.java
+++ src/main/java/sixgaezzang/sidepeek/comments/service/CommentService.java
@@ -1,14 +1,28 @@
 package sixgaezzang.sidepeek.comments.service;
 
+import static sixgaezzang.sidepeek.comments.exception.message.CommentErrorMessage.COMMENT_NOT_EXISTING;
+import static sixgaezzang.sidepeek.comments.exception.message.CommentErrorMessage.PARENT_COMMENT_NOT_EXISTING;
+import static sixgaezzang.sidepeek.common.util.validation.ValidationUtils.validateLoginId;
+import static sixgaezzang.sidepeek.common.util.validation.ValidationUtils.validateLoginIdEqualsOwnerId;
+import static sixgaezzang.sidepeek.projects.exception.message.ProjectErrorMessage.PROJECT_NOT_EXISTING;
+import static sixgaezzang.sidepeek.users.exception.message.UserErrorMessage.USER_NOT_EXISTING;
+
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sixgaezzang.sidepeek.comments.domain.Comment;
+import sixgaezzang.sidepeek.comments.dto.request.SaveCommentRequest;
+import sixgaezzang.sidepeek.comments.dto.request.UpdateCommentRequest;
 import sixgaezzang.sidepeek.comments.dto.response.CommentResponse;
 import sixgaezzang.sidepeek.comments.dto.response.ReplyResponse;
 import sixgaezzang.sidepeek.comments.repository.CommentRepository;
 import sixgaezzang.sidepeek.projects.domain.Project;
+import sixgaezzang.sidepeek.projects.repository.ProjectRepository;
+import sixgaezzang.sidepeek.users.domain.User;
+import sixgaezzang.sidepeek.users.repository.UserRepository;
 
 @Service
 @Transactional(readOnly = true)
@@ -16,6 +30,44 @@ import sixgaezzang.sidepeek.projects.domain.Project;
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final ProjectRepository projectRepository;
+
+    @Transactional
+    public Long save(Long loginId, SaveCommentRequest request) {
+        validateLoginId(loginId);
+        validateLoginIdEqualsOwnerId(loginId, request.ownerId());
+        User owner = userRepository.findById(request.ownerId())
+            .orElseThrow(() -> new EntityNotFoundException(USER_NOT_EXISTING));
+
+        Project project = null;
+        Comment parent = null;
+        if (Objects.isNull(request.parentId())) {
+            project = projectRepository.findById(request.projectId())
+                .orElseThrow(() -> new EntityNotFoundException(PROJECT_NOT_EXISTING));
+        } else {
+            parent = commentRepository.findById(request.parentId())
+                .orElseThrow(() -> new EntityNotFoundException(PARENT_COMMENT_NOT_EXISTING));
+        }
+
+        Comment comment = request.toEntity(project, parent, owner);
+
+        commentRepository.save(comment);
+
+        return Objects.nonNull(project) ? project.getId() : parent.getProject().getId();
+    }
+
+    @Transactional
+    public void update(Long loginId, Long commentId, UpdateCommentRequest request) {
+        validateLoginId(loginId);
+
+        Comment comment = commentRepository.findById(commentId)
+            .orElseThrow(() -> new EntityNotFoundException(COMMENT_NOT_EXISTING));
+
+        validateLoginIdEqualsOwnerId(loginId, comment.getUser().getId());
+
+        comment.update(request);
+    }
 
     public List<CommentResponse> findAll(Project project) {
         List<Comment> comments = commentRepository.findAll(project);
@@ -26,6 +78,10 @@ public class CommentService {
                 return CommentResponse.from(comment, isOwner, replies);
             })
             .toList();
+    }
+
+    public void delete(Long loginId, Long commentId) {
+        // TODO: 댓글 삭제 구현
     }
 
     private boolean isSameOwner(Comment comment, Project project) {
