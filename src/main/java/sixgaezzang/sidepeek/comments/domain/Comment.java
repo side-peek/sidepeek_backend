4f956bf54057b25ca41cedc8e8b73e514a8e866c
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-10 19:26:25 +0900

    feat: Comment 생성자에 필요한 유효성 검사 메서드 정의 및 적용

--- src/main/java/sixgaezzang/sidepeek/comments/domain/Comment.java
+++ src/main/java/sixgaezzang/sidepeek/comments/domain/Comment.java
@@ -2,6 +2,9 @@ package sixgaezzang.sidepeek.comments.domain;
 
 import static sixgaezzang.sidepeek.comments.exception.message.CommentErrorMessage.IS_ANONYMOUS_IS_NULL;
 import static sixgaezzang.sidepeek.comments.util.validation.CommentValidator.validateCommentContent;
+import static sixgaezzang.sidepeek.comments.util.validation.CommentValidator.validateIsAnonymous;
+import static sixgaezzang.sidepeek.projects.util.validation.ProjectValidator.validateProject;
+import static sixgaezzang.sidepeek.users.util.validation.UserValidator.validateUser;
 
 import io.jsonwebtoken.lang.Assert;
 import jakarta.persistence.Column;
@@ -42,6 +45,7 @@ public class Comment extends BaseTimeEntity {
     @JoinColumn(name = "user_id")
     private User user;
 
+    // TODO: @OnDelete(action = OnDeleteAction.CASCADE) 설정을 안해도 돌아간다. 필요할까!(고민중)
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
     private Comment parent;
@@ -53,13 +57,23 @@ public class Comment extends BaseTimeEntity {
     private String content;
 
     @Builder
-    public Comment(Project project, User user, Comment parent, boolean isAnonymous,
+    public Comment(Project project, User user, Comment parent, Boolean isAnonymous,
         String content) {
+        validateConstructorRequiredArguments(project, user, isAnonymous, content);
+
         this.project = project;
         this.user = user;
         this.parent = parent;
         this.isAnonymous = isAnonymous;
         this.content = content;
+    }
+
+    private static void validateConstructorRequiredArguments(Project project, User user, Boolean isAnonymous,
+                                                             String content) {
+        validateProject(project);
+        validateUser(user);
+        validateIsAnonymous(isAnonymous);
+        validateCommentContent(content);
     }
 
     // TODO: 업데이트 메서드는 request 인자를 풀어서 넣으면 더 확장성이 있다고 할 수 있을까?
--- src/main/java/sixgaezzang/sidepeek/comments/util/validation/CommentValidator.java
+++ src/main/java/sixgaezzang/sidepeek/comments/util/validation/CommentValidator.java
@@ -2,19 +2,42 @@ package sixgaezzang.sidepeek.comments.util.validation;
 
 import static sixgaezzang.sidepeek.comments.exception.message.CommentErrorMessage.CONTENT_IS_NULL;
 import static sixgaezzang.sidepeek.comments.exception.message.CommentErrorMessage.CONTENT_OVER_MAX_LENGTH;
+import static sixgaezzang.sidepeek.comments.exception.message.CommentErrorMessage.IS_ANONYMOUS_IS_NULL;
+import static sixgaezzang.sidepeek.comments.util.CommentConstant.CHILD_COMMENT_CANNOT_BE_PARENT;
+import static sixgaezzang.sidepeek.comments.util.CommentConstant.COMMENT_ID_IS_NULL;
 import static sixgaezzang.sidepeek.comments.util.CommentConstant.MAX_CONTENT_LENGTH;
 import static sixgaezzang.sidepeek.common.util.validation.ValidationUtils.validateMaxLength;
 import static sixgaezzang.sidepeek.common.util.validation.ValidationUtils.validateNotBlank;
 
+import io.jsonwebtoken.lang.Assert;
+import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import sixgaezzang.sidepeek.comments.domain.Comment;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class CommentValidator {
 
+    // Common
+    public static void validateCommentId(Long commentId) {
+        Assert.notNull(commentId, COMMENT_ID_IS_NULL);
+    }
+
+    public static void validateParentCommentHasParent(Comment parent) {
+        if (Objects.nonNull(parent.getParent())) {
+            throw new IllegalArgumentException(CHILD_COMMENT_CANNOT_BE_PARENT);
+        }
+    }
+
+    // Content
     public static void validateCommentContent(String content) {
         validateNotBlank(content, CONTENT_IS_NULL);
         validateMaxLength(content, MAX_CONTENT_LENGTH, CONTENT_OVER_MAX_LENGTH);
+    }
+
+    // IsAnonymous
+    public static void validateIsAnonymous(Boolean isAnonymous) {
+        Assert.notNull(isAnonymous, IS_ANONYMOUS_IS_NULL);
     }
 
 }
