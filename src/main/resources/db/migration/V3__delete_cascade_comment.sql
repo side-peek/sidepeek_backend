151e1ccbbf725cbd31b6b6ec8a3cf09affde8172
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-10 19:00:24 +0900

    chore: comments 테이블 parent_id FK ON DELETE CASCADE 설정

---
+++ src/main/resources/db/migration/V3__delete_cascade_comment.sql
@@ -0,0 +1,9 @@
+-- COMMENT
+ALTER TABLE comments
+    DROP FOREIGN KEY comments_ibfk_3;
+
+ALTER TABLE comments
+    ADD CONSTRAINT
+        FOREIGN KEY (parent_id)
+            REFERENCES comments (id)
+            ON DELETE CASCADE;
