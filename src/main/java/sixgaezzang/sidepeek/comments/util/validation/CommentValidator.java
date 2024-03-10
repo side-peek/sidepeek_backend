1e871ae82ec0b64a74f6418ec87bdec57c075601
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-10 22:14:59 +0900

    feat: validateSaveCommentRequest 구현

--- src/main/java/sixgaezzang/sidepeek/comments/util/validation/CommentValidator.java
+++ src/main/java/sixgaezzang/sidepeek/comments/util/validation/CommentValidator.java
@@ -5,15 +5,18 @@ import static sixgaezzang.sidepeek.comments.exception.message.CommentErrorMessag
 import static sixgaezzang.sidepeek.comments.exception.message.CommentErrorMessage.CONTENT_IS_NULL;
 import static sixgaezzang.sidepeek.comments.exception.message.CommentErrorMessage.CONTENT_OVER_MAX_LENGTH;
 import static sixgaezzang.sidepeek.comments.exception.message.CommentErrorMessage.IS_ANONYMOUS_IS_NULL;
+import static sixgaezzang.sidepeek.comments.exception.message.CommentErrorMessage.PROJECT_ID_AND_PARENT_ID_IS_NULL;
 import static sixgaezzang.sidepeek.comments.util.CommentConstant.MAX_CONTENT_LENGTH;
 import static sixgaezzang.sidepeek.common.util.validation.ValidationUtils.validateMaxLength;
 import static sixgaezzang.sidepeek.common.util.validation.ValidationUtils.validateNotBlank;
+import static sixgaezzang.sidepeek.projects.util.validation.ProjectValidator.validateOwnerId;
 
 import io.jsonwebtoken.lang.Assert;
 import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import sixgaezzang.sidepeek.comments.domain.Comment;
+import sixgaezzang.sidepeek.comments.dto.request.SaveCommentRequest;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class CommentValidator {
@@ -26,6 +29,13 @@ public final class CommentValidator {
     public static void validateParentCommentHasParent(Comment parent) {
         if (Objects.nonNull(parent.getParent())) {
             throw new IllegalArgumentException(CHILD_COMMENT_CANNOT_BE_PARENT);
+        }
+    }
+
+    public static void validateSaveCommentRequest(SaveCommentRequest request) {
+        validateOwnerId(request.ownerId());
+        if (Objects.isNull(request.projectId()) && Objects.isNull(request.parentId())) {
+            throw new IllegalArgumentException(PROJECT_ID_AND_PARENT_ID_IS_NULL);
         }
     }
 
