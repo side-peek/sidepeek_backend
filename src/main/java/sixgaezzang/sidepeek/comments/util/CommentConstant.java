508ea4e8caad718905b4ab20d41b821843354374
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-11 10:23:03 +0900

    refactor: 댓글 내용 제한 100자로 변경

--- src/main/java/sixgaezzang/sidepeek/comments/util/CommentConstant.java
+++ src/main/java/sixgaezzang/sidepeek/comments/util/CommentConstant.java
@@ -6,6 +6,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class CommentConstant {
 
-    public static final int MAX_CONTENT_LENGTH = 300;
+    public static final int MAX_CONTENT_LENGTH = 100;
 
 }
--- src/main/resources/db/migration/V3__delete_cascade_comment.sql
+++ src/main/resources/db/migration/V3__delete_cascade_comment.sql
@@ -1,5 +1,8 @@
 -- COMMENT
 ALTER TABLE comments
+    MODIFY COLUMN content VARCHAR(100) NOT NULL;
+
+ALTER TABLE comments
     DROP FOREIGN KEY comments_ibfk_3;
 
 ALTER TABLE comments
