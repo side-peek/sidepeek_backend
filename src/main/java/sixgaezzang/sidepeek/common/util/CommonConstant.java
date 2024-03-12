9b025fed5fcd0b4946132b70a5d4edd47d559836
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-12 18:41:43 +0900

    feat: GITHUB_URL 상수 추가

--- src/main/java/sixgaezzang/sidepeek/common/util/CommonConstant.java
+++ src/main/java/sixgaezzang/sidepeek/common/util/CommonConstant.java
@@ -9,6 +9,9 @@ public final class CommonConstant {
     public static final int MAX_TEXT_LENGTH = 21_844;
     public static final String BLANK_STRING = "";
 
+    // Github
+    public static final String GITHUB_URL = "https://github.com";
+
     // TechStack
     public static final int MAX_TECH_STACK_COUNT = 30;
     public static final int MAX_CATEGORY_LENGTH = 50;
