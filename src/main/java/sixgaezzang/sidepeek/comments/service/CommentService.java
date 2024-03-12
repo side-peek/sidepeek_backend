ba2b6995458608f39ffc3160de9ad9c54a033258
EomYerim <djadpfa1002@naver.com>
2024-03-13 03:37:52 +0900

    refactor: 댓글 목록 조회 메서드 반환형 변경 및 댓글수 계산 로직 추가

--- src/main/java/sixgaezzang/sidepeek/comments/service/CommentService.java
+++ src/main/java/sixgaezzang/sidepeek/comments/service/CommentService.java
@@ -1,11 +1,14 @@
 package sixgaezzang.sidepeek.comments.service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sixgaezzang.sidepeek.comments.domain.Comment;
 import sixgaezzang.sidepeek.comments.dto.response.CommentResponse;
+import sixgaezzang.sidepeek.comments.dto.response.CommentWithCountResponse;
 import sixgaezzang.sidepeek.comments.dto.response.ReplyResponse;
 import sixgaezzang.sidepeek.comments.repository.CommentRepository;
 import sixgaezzang.sidepeek.projects.domain.Project;
@@ -17,15 +20,26 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
 
-    public List<CommentResponse> findAll(Project project) {
+    public CommentWithCountResponse findAll(Project project) {
         List<Comment> comments = commentRepository.findAll(project);
-        return comments.stream()
+        AtomicLong commentCount = new AtomicLong((long) comments.size());    // 댓글 개수
+
+        if (comments.isEmpty()) {
+            return CommentWithCountResponse.from(null, commentCount.get()); // 댓글이 없는 경우 null 반환
+        }
+
+        List<CommentResponse> results = comments.stream()
             .map(comment -> {
                 boolean isOwner = isSameOwner(comment, project);
                 List<ReplyResponse> replies = mapReplies(comment);
+                if (Objects.nonNull(replies)) {
+                    commentCount.addAndGet(replies.size()); // 대댓글 개수 추가
+                }
                 return CommentResponse.from(comment, isOwner, replies);
             })
             .toList();
+
+        return CommentWithCountResponse.from(results, commentCount.get());
     }
 
     private boolean isSameOwner(Comment comment, Project project) {
