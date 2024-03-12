40cd832a48fadabe9364f7c80213e6b2fdae7712
EomYerim <djadpfa1002@naver.com>
2024-03-13 08:23:53 +0900

    feat: like 유효성 검사 validator 생성

---
+++ src/main/java/sixgaezzang/sidepeek/like/util/validation/LikeValidator.java
@@ -0,0 +1,19 @@
+package sixgaezzang.sidepeek.like.util.validation;
+
+import static sixgaezzang.sidepeek.like.exception.message.LikeErrorMessage.PROJECT_ID_IS_NULL;
+
+import java.util.Objects;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import sixgaezzang.sidepeek.like.dto.request.LikeRequest;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class LikeValidator {
+
+    public static void validateLikeRequest(LikeRequest request) {
+        if (Objects.isNull(request.projectId())) {
+            throw new IllegalArgumentException(PROJECT_ID_IS_NULL);
+        }
+    }
+
+}
