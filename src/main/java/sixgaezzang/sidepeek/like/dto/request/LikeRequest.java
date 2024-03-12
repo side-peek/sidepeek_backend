99b172ef43c834cb6656c1685d8f7a0a80a44999
EomYerim <djadpfa1002@naver.com>
2024-03-13 08:47:17 +0900

    style: like 요청 dto checkstyle 오류 해결

--- src/main/java/sixgaezzang/sidepeek/like/dto/request/LikeRequest.java
+++ src/main/java/sixgaezzang/sidepeek/like/dto/request/LikeRequest.java
@@ -5,13 +5,12 @@ import static sixgaezzang.sidepeek.common.util.CommonConstant.MIN_ID;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 
+// TODO : 로그인한 사용자로 좋아요할 수 있는데, Long userId가 필요할까?
 public record LikeRequest(
     // project id
     @Schema(description = "프로젝트 식별자(댓글인 경우 필수)", example = "1")
     @Min(value = MIN_ID, message = "프로젝트 id는 " + MIN_ID + "보다 작을 수 없습니다.")
     Long projectId
-
-    // TODO : 로그인한 사용자로 좋아요할 수 있는데, Long userId가 필요할까?
 ) {
 
 }
