2dcd02aed4f4983b439a8b68e90b6f6dcf246e1a
EomYerim <djadpfa1002@naver.com>
2024-03-13 01:57:53 +0900

    refactor: 프로젝트 조회에 대한 다중 필터 및 커서 기반 페에지네이션 응답 dto에 totalElements 추가

--- src/main/java/sixgaezzang/sidepeek/projects/dto/response/CursorPaginationResponse.java
+++ src/main/java/sixgaezzang/sidepeek/projects/dto/response/CursorPaginationResponse.java
@@ -12,22 +12,22 @@ public record CursorPaginationResponse<T>(
     @Schema(description = "프로젝트 목록")
     List<T> content,
 
-    // TODO: 검색된 전체 데이터의 개수
-//    @Schema(description = "전체 데이터 개수")
-//    Integer totalElements,
+    @Schema(description = "전체 데이터 개수")
+    long totalElements,
 
     @Schema(description = "현재 페이지의 데이터 개수")
-    Integer numberOfElements,
+    int numberOfElements,
 
     @Schema(description = "다음 페이지 존재 여부")
     boolean hasNext
 ) {
 
     public static <T> CursorPaginationResponse from(List<T> projects,
+        long totalElements,
         boolean hasNext) {
         return CursorPaginationResponse.builder()
             .content(new ArrayList<>(projects))
-//            .totalElements()
+            .totalElements(totalElements)
             .numberOfElements(projects.size())
             .hasNext(hasNext)
             .build();
