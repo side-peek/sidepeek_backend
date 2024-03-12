8cc460a854813f28527d15de9b85783bab138053
EomYerim <djadpfa1002@naver.com>
2024-03-12 23:50:13 +0900

    feat: 정렬 조건(다중 필터) enum 생성

---
+++ src/main/java/sixgaezzang/sidepeek/projects/dto/request/SortType.java
@@ -0,0 +1,25 @@
+package sixgaezzang.sidepeek.projects.dto.request;
+
+// TODO: 대문자 변경 리팩토링 필요!
+//public enum SortType {
+//    createdAt(1),
+//    like(2),
+//    view(3);
+//
+//    private final int sortNum;
+//
+//    SortType(int sortNum) {
+//        this.sortNum = sortNum;
+//    }
+//
+//    public int getSortNum() {
+//        return sortNum;
+//    }
+//
+//}
+
+public enum SortType {
+    createdAt,
+    like,
+    view
+}
