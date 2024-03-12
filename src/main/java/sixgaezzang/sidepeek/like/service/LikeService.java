0b3326d965705e695ab7cf538fb1f28bf1acbc66
EomYerim <djadpfa1002@naver.com>
2024-03-13 07:59:17 +0900

    feat: like 좋아요 토글 관련 service 및 메서드 생성

---
+++ src/main/java/sixgaezzang/sidepeek/like/service/LikeService.java
@@ -0,0 +1,54 @@
+package sixgaezzang.sidepeek.like.service;
+
+import static sixgaezzang.sidepeek.projects.exception.message.ProjectErrorMessage.PROJECT_NOT_EXISTING;
+import static sixgaezzang.sidepeek.users.exception.message.UserErrorMessage.USER_NOT_EXISTING;
+
+import jakarta.persistence.EntityNotFoundException;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sixgaezzang.sidepeek.like.domain.Like;
+import sixgaezzang.sidepeek.like.dto.request.LikeRequest;
+import sixgaezzang.sidepeek.like.dto.response.LikeResponse;
+import sixgaezzang.sidepeek.like.repository.LikeRepository;
+import sixgaezzang.sidepeek.projects.domain.Project;
+import sixgaezzang.sidepeek.projects.repository.ProjectRepository;
+import sixgaezzang.sidepeek.users.domain.User;
+import sixgaezzang.sidepeek.users.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class LikeService {
+
+    private final LikeRepository likeRepository;
+    private final UserRepository userRepository;
+    private final ProjectRepository projectRepository;
+
+    @Transactional
+    public LikeResponse toggle(Long userId, LikeRequest request) {
+
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> new EntityNotFoundException(USER_NOT_EXISTING));
+
+        Project project = projectRepository.findById(request.projectId())
+            .orElseThrow(() -> new EntityNotFoundException(PROJECT_NOT_EXISTING));
+
+        Optional<Like> existingLike = likeRepository.findByUserAndProject(user, project);
+
+        if (existingLike.isPresent()) {  // 좋아요 삭제(취소)
+            likeRepository.delete(existingLike.get());
+            return new LikeResponse(false);
+        }
+
+        Like like = Like.builder()  // 좋아요 생성
+            .user(user)
+            .project(project)
+            .build();
+
+        likeRepository.save(like);
+
+        return new LikeResponse(true);
+    }
+}
