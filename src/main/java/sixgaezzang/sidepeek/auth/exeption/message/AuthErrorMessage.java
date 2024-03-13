54a8abda229e16a997838501673f1cf6735f4f72
uijin-j <uijin0531@gmail.com>
2024-03-13 16:30:47 +0900

    feat: Auth 관련 예외 메시지 추가

--- src/main/java/sixgaezzang/sidepeek/auth/exeption/message/AuthErrorMessage.java
+++ src/main/java/sixgaezzang/sidepeek/auth/exeption/message/AuthErrorMessage.java
@@ -11,5 +11,6 @@ public class AuthErrorMessage {
     public static final String REFRESH_TOKEN_IS_NULL = "Refresh Token을 입력해주세요.";
     public static final String TOKEN_IS_INVALID = "유효하지 않은 토큰입니다.";
     public static final String PASSWORD_NOT_MATCH = "비밀번호가 일치하지 않습니다.";
+    public static final String OAUTH_USER_TYPE_IS_INVALID = "유효하지 않은 OAuth 사용자 타입입니다.";
 
 }
