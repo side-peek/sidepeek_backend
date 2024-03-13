3015d315c19bb1bf635909750fe299e354f6520c
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-13 14:09:47 +0900

    feat: ProjectBannerResponse에 불필요한 인자 삭제

--- src/main/java/sixgaezzang/sidepeek/projects/dto/response/ProjectBannerResponse.java
+++ src/main/java/sixgaezzang/sidepeek/projects/dto/response/ProjectBannerResponse.java
@@ -14,11 +14,7 @@ public record ProjectBannerResponse(
     @Schema(description = "프로젝트 부제목", example = "요즘 사이드 플젝 뭐함? 사이드픽 \uD83D\uDC40")
     String subName,
     @Schema(description = "프로젝트 썸네일 이미지 URL", example = "https://sidepeek.image/imageeUrl")
-    String thumbnailUrl,
-    @Schema(description = "프로젝트 조회수", example = "20")
-    Long viewCount,
-    @Schema(description = "프로젝트 좋아요수", example = "7")
-    Long likeCount
+    String thumbnailUrl
 ) {
 
     public static ProjectBannerResponse from(Project project) {
@@ -27,8 +23,6 @@ public record ProjectBannerResponse(
             .name(project.getName())
             .subName(project.getSubName())
             .thumbnailUrl(project.getThumbnailUrl())
-            .viewCount(project.getViewCount())
-            .likeCount(project.getLikeCount())
             .build();
     }
 
