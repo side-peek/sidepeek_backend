48c07fd62b3b2d09c77824275c94261dc589686a
EomYerim <djadpfa1002@naver.com>
2024-03-14 03:15:29 +0900

    feat: like 좋아요 및 좋아요 취소 controllerDocs 생성

--- src/main/java/sixgaezzang/sidepeek/common/doc/LikeControllerDoc.java
+++ src/main/java/sixgaezzang/sidepeek/common/doc/LikeControllerDoc.java
@@ -16,14 +16,23 @@ import sixgaezzang.sidepeek.like.dto.response.LikeResponse;
 @Tag(name = "Like", description = "좋아요 API")
 public interface LikeControllerDoc {
 
-    @Operation(summary = "좋아요 생성 및 삭제(토글)")
+    @Operation(summary = "좋아요")
     @ApiResponses({
         @ApiResponse(responseCode = "200", description = "OK", useReturnTypeSchema = true),
         @ApiResponse(responseCode = "400", description = "BAD_REQUEST", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
         @ApiResponse(responseCode = "401", description = "UNAUTHORIZED", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
         @ApiResponse(responseCode = "404", description = "NOT_FOUND", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
-    @Parameter(name = "projectId", description = "좋아요할 프로젝트 식별자", example = "1", in = ParameterIn.PATH)
-    ResponseEntity<LikeResponse> toggle(@Parameter(hidden = true) Long loginId,
-        LikeRequest request);
+    ResponseEntity<LikeResponse> save(@Parameter(hidden = true) Long loginId, LikeRequest request);
+
+    @Operation(summary = "좋아요 취소")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "OK", useReturnTypeSchema = true),
+        @ApiResponse(responseCode = "400", description = "BAD_REQUEST", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+        @ApiResponse(responseCode = "401", description = "UNAUTHORIZED", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+        @ApiResponse(responseCode = "404", description = "NOT_FOUND", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    @Parameter(name = "likeId", description = "삭제할 좋아요 식별자", example = "1", in = ParameterIn.PATH)
+    ResponseEntity<Void> delete(@Parameter(hidden = true) Long loginId, Long likeId);
+
 }
