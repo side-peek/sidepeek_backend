ff0b8ce9a00bd0064ea6a0f537b6258aacbd03e6
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-14 23:07:46 +0900

    feat: ProjectControllerDoc 설명 추가

--- src/main/java/sixgaezzang/sidepeek/common/doc/ProjectControllerDoc.java
+++ src/main/java/sixgaezzang/sidepeek/common/doc/ProjectControllerDoc.java
@@ -24,7 +24,7 @@ import sixgaezzang.sidepeek.projects.dto.response.ProjectResponse;
 @Tag(name = "Project", description = "프로젝트 API")
 public interface ProjectControllerDoc {
 
-    @Operation(summary = "프로젝트 생성")
+    @Operation(summary = "프로젝트 생성", description = "로그인 필수")
     @ApiResponses({
         @ApiResponse(responseCode = "201", description = "CREATED", useReturnTypeSchema = true),
         @ApiResponse(responseCode = "400", description = "BAD_REQUEST", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
@@ -33,7 +33,7 @@ public interface ProjectControllerDoc {
     ResponseEntity<ProjectResponse> save(@Parameter(hidden = true) Long loginId,
                                          SaveProjectRequest request);
 
-    @Operation(summary = "프로젝트 상세 조회")
+    @Operation(summary = "프로젝트 상세 조회", description = "로그인 선택")
     @ApiResponses({
         @ApiResponse(responseCode = "200", description = "OK", useReturnTypeSchema = true),
         @ApiResponse(responseCode = "404", description = "NOT_FOUND", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
@@ -41,7 +41,7 @@ public interface ProjectControllerDoc {
     @Parameter(name = "id", description = "조회할 프로젝트 식별자", in = ParameterIn.PATH)
     ResponseEntity<ProjectResponse> getById(Long id);
 
-    @Operation(summary = "프로젝트 전체 조회")
+    @Operation(summary = "프로젝트 전체 조회", description = "프로젝트 게시글 목록을 조건에 따라 조회, 로그인 선택")
     @ApiResponses({
         @ApiResponse(responseCode = "200", description = "OK", useReturnTypeSchema = true)
     })
@@ -49,13 +49,13 @@ public interface ProjectControllerDoc {
         @Parameter(hidden = true) Long loginId,
         @Valid @ModelAttribute CursorPaginationInfoRequest pageable);
 
-    @Operation(summary = "금주의 인기 프로젝트 조회(배너용)", description = "주간 좋아요 기록을 통해 프로젝트 인기순으로 정렬")
+    @Operation(summary = "지난 주 인기 프로젝트 조회", description = "지난 주 좋아요를 많이 받은 순으로 최대 5개 프로젝트 목록 조회, 로그인 선택")
     @ApiResponses({
         @ApiResponse(responseCode = "200", description = "OK", useReturnTypeSchema = true)
     })
     ResponseEntity<List<ProjectBannerResponse>> getAllPopularThisWeek();
 
-    @Operation(summary = "프로젝트 수정", description = "프로젝트 작성자와 멤버만 수정이 가능합니다.")
+    @Operation(summary = "프로젝트 수정", description = "작성자와 등록된 프로젝트 회원 멤버만 수정 가능, 로그인 필수")
     @ApiResponses({
         @ApiResponse(responseCode = "200", description = "OK", useReturnTypeSchema = true),
         @ApiResponse(responseCode = "400", description = "BAD_REQUEST", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
@@ -67,7 +67,7 @@ public interface ProjectControllerDoc {
     ResponseEntity<ProjectResponse> update(@Parameter(hidden = true) Long loginId, Long projectId,
                                            UpdateProjectRequest request);
 
-    @Operation(summary = "프로젝트 삭제", description = "프로젝트 작성자만 삭제가 가능합니다.")
+    @Operation(summary = "프로젝트 삭제", description = "프로젝트 작성자만 삭제 가능, 로그인 필수")
     @ApiResponses({
         @ApiResponse(responseCode = "204", description = "NO_CONTENT"),
         @ApiResponse(responseCode = "401", description = "UNAUTHORIZED", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
