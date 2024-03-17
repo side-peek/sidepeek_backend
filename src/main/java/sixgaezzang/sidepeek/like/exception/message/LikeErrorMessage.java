bf5d98ebd73fbe6c898e6cde35d0907a743c5686
EomYerim <djadpfa1002@naver.com>
2024-03-17 15:21:46 +0900

    feat: 로그인한 사용자와 좋아요한 사용자가 다를 경우에 대한 유효성 검사 및 에러 메세지 추가

--- src/main/java/sixgaezzang/sidepeek/like/exception/message/LikeErrorMessage.java
+++ src/main/java/sixgaezzang/sidepeek/like/exception/message/LikeErrorMessage.java
@@ -14,4 +14,7 @@ public final class LikeErrorMessage {
     // projectId
     public static final String PROJECT_ID_IS_NULL = "프로젝트 ID를 입력해주세요.";
 
+    // userId
+    public static final String LIKE_OWNER_ID_NOT_EQUALS_LOGIN_ID = "좋아요한 회원의 Id가 로그인한 회원 Id와 일치하지 않습니다.";
+
 }
--- src/main/java/sixgaezzang/sidepeek/like/util/validation/LikeValidator.java
+++ src/main/java/sixgaezzang/sidepeek/like/util/validation/LikeValidator.java
@@ -1,11 +1,14 @@
 package sixgaezzang.sidepeek.like.util.validation;
 
 import static sixgaezzang.sidepeek.like.exception.message.LikeErrorMessage.LIKE_ID_IS_NULL;
+import static sixgaezzang.sidepeek.like.exception.message.LikeErrorMessage.LIKE_OWNER_ID_NOT_EQUALS_LOGIN_ID;
 import static sixgaezzang.sidepeek.like.exception.message.LikeErrorMessage.PROJECT_ID_IS_NULL;
+import static sixgaezzang.sidepeek.projects.util.validation.ProjectValidator.validateOwnerId;
 
 import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.util.Assert;
 import sixgaezzang.sidepeek.like.dto.request.LikeRequest;
 
@@ -20,6 +23,13 @@ public final class LikeValidator {
     public static void validateLikeRequest(LikeRequest request) {
         if (Objects.isNull(request.projectId())) {
             throw new IllegalArgumentException(PROJECT_ID_IS_NULL);
+        }
+    }
+
+    public static void validateLoginIdEqualsLikeOwnerId(Long loginId, Long ownerId) {
+        validateOwnerId(ownerId);
+        if (!loginId.equals(ownerId)) {
+            throw new AccessDeniedException(LIKE_OWNER_ID_NOT_EQUALS_LOGIN_ID);
         }
     }
 
