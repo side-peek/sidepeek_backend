539f3a842a4490d25e0043f4489372ceb6195db7
EomYerim <djadpfa1002@naver.com>
2024-03-19 01:02:55 +0900

    refactor: redisTemplate에 트랜잭션 적용 및 직렬화 설정 추가

--- src/main/java/sixgaezzang/sidepeek/config/RedisConfig.java
+++ src/main/java/sixgaezzang/sidepeek/config/RedisConfig.java
@@ -8,6 +8,9 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import sixgaezzang.sidepeek.config.properties.RedisProperties;
 
 @Configuration
@@ -28,6 +31,14 @@ public class RedisConfig {
     public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        template.setEnableTransactionSupport(true);
         return template;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new JpaTransactionManager();
     }
 }
