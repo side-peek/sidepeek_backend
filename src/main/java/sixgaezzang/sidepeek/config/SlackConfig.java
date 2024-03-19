d4bd9b0093947f2343bbd0218ee847ff19690c38
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-19 14:47:47 +0900

    feat: SlackConfig 로직 수정

--- src/main/java/sixgaezzang/sidepeek/config/SlackConfig.java
+++ src/main/java/sixgaezzang/sidepeek/config/SlackConfig.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import sixgaezzang.sidepeek.common.util.component.SlackClient;
+import sixgaezzang.sidepeek.config.properties.SentryAlertProperties;
 import sixgaezzang.sidepeek.config.properties.SlackProperties;
 
 //@Profile({"dev", "prod"})
@@ -16,7 +17,7 @@ public class SlackConfig {
     private final SlackProperties slackProperties;
 
     @Bean
-    SlackClient slackClient(SlackProperties slackProperties) {
-        return new SlackClient(Slack.getInstance(), slackProperties);
+    SlackClient slackClient(SlackProperties slackProperties, SentryAlertProperties sentryAlertProperties) {
+        return new SlackClient(Slack.getInstance(), slackProperties, sentryAlertProperties);
     }
 }
