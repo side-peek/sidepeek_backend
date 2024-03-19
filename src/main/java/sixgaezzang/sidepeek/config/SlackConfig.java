a3e793c05991c5d985333633c7d7d131a9db052d
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-19 11:57:25 +0900

    fix: Bean 애너테이션 추가

--- src/main/java/sixgaezzang/sidepeek/config/SlackConfig.java
+++ src/main/java/sixgaezzang/sidepeek/config/SlackConfig.java
@@ -3,6 +3,7 @@ package sixgaezzang.sidepeek.config;
 import com.slack.api.Slack;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import sixgaezzang.sidepeek.common.util.component.SlackClient;
 import sixgaezzang.sidepeek.config.properties.SlackProperties;
@@ -14,6 +15,7 @@ public class SlackConfig {
 
     private final SlackProperties slackProperties;
 
+    @Bean
     SlackClient slackClient(SlackProperties slackProperties) {
         return new SlackClient(Slack.getInstance(), slackProperties);
     }
