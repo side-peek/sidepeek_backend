9f629075965dab253a93d1d8a33e53cd7155bfcb
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-19 11:45:08 +0900

    feat: SlackProperties 설정

--- src/main/java/sixgaezzang/sidepeek/config/properties/SlackProperties.java
+++ src/main/java/sixgaezzang/sidepeek/config/properties/SlackProperties.java
@@ -2,6 +2,7 @@ package sixgaezzang.sidepeek.config.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+//@Profile({"dev", "prod"})
 @ConfigurationProperties(prefix = "slack.webhook")
 public record SlackProperties(
     String url
