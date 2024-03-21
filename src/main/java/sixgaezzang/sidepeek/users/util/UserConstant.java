a493a5923f9cc6b685fa6741b4fa61dba16d5c67
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-21 11:32:26 +0900

    fix: MAX_INTRODUCTION_LENGTH DB 설정과 동일하게 변경

--- src/main/java/sixgaezzang/sidepeek/users/util/UserConstant.java
+++ src/main/java/sixgaezzang/sidepeek/users/util/UserConstant.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 public final class UserConstant {
 
     public static final int MAX_NICKNAME_LENGTH = 20;
-    public static final int MAX_INTRODUCTION_LENGTH = 300;
+    public static final int MAX_INTRODUCTION_LENGTH = 100;
     public static final int MAX_CAREER_LENGTH = 30;
     public static final int MAX_JOB_LENGTH = 30;
     public static final int MAX_EMAIL_LENGTH = 50;
