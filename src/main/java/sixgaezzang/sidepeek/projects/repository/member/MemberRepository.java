8e9b583b039f113b95f6131ee1e021ade8aabefa
EomYerim <djadpfa1002@naver.com>
2024-03-22 06:23:12 +0900

    refactor: member repository 사용하지 않는 findAllWithUser 메서드 제거 및 관련 파일 제거

--- src/main/java/sixgaezzang/sidepeek/projects/repository/member/MemberRepository.java
+++ src/main/java/sixgaezzang/sidepeek/projects/repository/member/MemberRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import sixgaezzang.sidepeek.projects.domain.Project;
 import sixgaezzang.sidepeek.projects.domain.member.Member;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findAllByProject(Project project);
 
