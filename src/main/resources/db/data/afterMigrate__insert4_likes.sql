86396b2d89392e49f99c9cfaf554775d003e6e03
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-03-22 17:46:23 +0900

    refactor: like 더미데이터 생성

---
+++ src/main/resources/db/data/afterMigrate__insert4_likes.sql
@@ -0,0 +1,111 @@
+-- LIKES
+insert into likes(id, user_id, project_id)
+values (1, 1, 1);
+insert into likes(id, user_id, project_id)
+values (2, 2, 2);
+insert into likes(id, user_id, project_id)
+values (3, 3, 3);
+insert into likes(id, user_id, project_id)
+values (4, 4, 4);
+insert into likes(id, user_id, project_id)
+values (5, 5, 5);
+insert into likes(id, user_id, project_id)
+values (6, 6, 6);
+insert into likes(id, user_id, project_id)
+values (7, 7, 7);
+insert into likes(id, user_id, project_id)
+values (8, 8, 8);
+insert into likes(id, user_id, project_id)
+values (9, 9, 9);
+insert into likes(id, user_id, project_id)
+values (10, 10, 10);
+insert into likes(id, user_id, project_id)
+values (11, 11, 1);
+insert into likes(id, user_id, project_id)
+values (12, 12, 2);
+insert into likes(id, user_id, project_id)
+values (13, 1, 3);
+insert into likes(id, user_id, project_id)
+values (14, 2, 4);
+insert into likes(id, user_id, project_id)
+values (15, 3, 5);
+insert into likes(id, user_id, project_id)
+values (16, 4, 6);
+insert into likes(id, user_id, project_id)
+values (17, 5, 7);
+insert into likes(id, user_id, project_id)
+values (18, 6, 8);
+insert into likes(id, user_id, project_id)
+values (19, 7, 9);
+insert into likes(id, user_id, project_id)
+values (20, 8, 1);
+insert into likes(id, user_id, project_id)
+values (21, 9, 2);
+insert into likes(id, user_id, project_id)
+values (22, 10, 3);
+insert into likes(id, user_id, project_id)
+values (23, 11, 4);
+insert into likes(id, user_id, project_id)
+values (24, 12, 5);
+insert into likes(id, user_id, project_id)
+values (25, 1, 6);
+insert into likes(id, user_id, project_id)
+values (26, 2, 7);
+insert into likes(id, user_id, project_id)
+values (27, 3, 8);
+insert into likes(id, user_id, project_id)
+values (28, 4, 1);
+insert into likes(id, user_id, project_id)
+values (29, 5, 2);
+insert into likes(id, user_id, project_id)
+values (30, 6, 3);
+insert into likes(id, user_id, project_id)
+values (31, 7, 4);
+insert into likes(id, user_id, project_id)
+values (32, 8, 5);
+insert into likes(id, user_id, project_id)
+values (33, 9, 6);
+insert into likes(id, user_id, project_id)
+values (34, 10, 7);
+insert into likes(id, user_id, project_id)
+values (35, 11, 1);
+insert into likes(id, user_id, project_id)
+values (36, 12, 2);
+insert into likes(id, user_id, project_id)
+values (37, 1, 3);
+insert into likes(id, user_id, project_id)
+values (38, 2, 4);
+insert into likes(id, user_id, project_id)
+values (39, 3, 5);
+insert into likes(id, user_id, project_id)
+values (40, 4, 6);
+insert into likes(id, user_id, project_id)
+values (41, 5, 1);
+insert into likes(id, user_id, project_id)
+values (42, 6, 2);
+insert into likes(id, user_id, project_id)
+values (43, 7, 3);
+insert into likes(id, user_id, project_id)
+values (44, 8, 4);
+insert into likes(id, user_id, project_id)
+values (45, 9, 5);
+insert into likes(id, user_id, project_id)
+values (46, 10, 1);
+insert into likes(id, user_id, project_id)
+values (47, 11, 2);
+insert into likes(id, user_id, project_id)
+values (48, 12, 3);
+insert into likes(id, user_id, project_id)
+values (49, 1, 4);
+insert into likes(id, user_id, project_id)
+values (50, 2, 5);
+insert into likes(id, user_id, project_id)
+values (51, 3, 6);
+insert into likes(id, user_id, project_id)
+values (52, 4, 7);
+insert into likes(id, user_id, project_id)
+values (53, 5, 8);
+insert into likes(id, user_id, project_id)
+values (54, 6, 9);
+insert into likes(id, user_id, project_id)
+values (55, 7, 10);
